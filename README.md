99eb4ae5c5378362400163510b1358e336762f80
beesoc <beesoc@hotmail.com>
2023-04-29 04:11:52 -0500

    commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # beesoc-menu
 Source for my custom BASH menu system
+
+First of all, I am not the author of ANY of these tools that this menu system is installing, configuring or running. I have just put together this BASH menu system to help some non-technical friends utilize Linux, Pwnagotchi's and some pretty sweet Linux tools.
+
+Setup - To install Beesoc's Easy Linux menu system, run 
--- support/install-sysinfo.sh
+++ support/install-sysinfo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -e
+#set -e
 # shellcheck source=.envrc
 clear
-source ../.envrc
-# shellcheck source="support/Banner_func.sh"
-source "Banner_func.sh"
+source ${scripts_dir}/support/Banner_func.sh
+source ${scripts_dir}/.envrc
+# shellcheck source=./Banner_func.sh
+
 # Display hostname and username
 printf "${CY}TZ: ${WT}$(timedatectl | grep "Time zone:" | awk '{print $3}')            ${CY}Time: ${WT}$(date +%I:%M:%S\ ${GN}%p)        ${CY}Username: ${GN}$(whoami)""${WT}@""${GN}$(hostname)${NC}\\n"
 
@@ -14,7 +15,7 @@ battery="$(acpi -b | awk '/Battery 0/ {print $3 $4 $5}')"
 batt_per="$(acpi -b | awk '/Battery 0/ {print $4}')"
 battery_state="$(acpi -b | awk '/Battery 0/ {print $3}')"
 # Display Battery info
-printf "\\n${CY}Your battery state is currently ${WT}${battery_state}${CY} and your machine has ${WT}${batt_per}% battery remaining.\\n"
+printf "\\n${CY}Your battery state is currently${WT} ${battery_state}${CY} and your machine has${WT} ${batt_per}% battery remaining.\\n"
 printf "\\n"
 
 # Display disk usage
@@ -32,6 +33,12 @@ printf "${CY}Disk Usage: ${WT}${disk_usage}%   ${CY}IP:${WT} ${ip_address}    ${
 updates=$(apt list --upgradable 2>/dev/null | wc -l)
 security_updates=$(apt list --upgradable 2>/dev/null | grep -i security | wc -l)
 printf "${CY}# Updates Available: ${WT}$updates     |    ${CY}# Security Updates Available: ${RED}$security_updates"
+
+	lsb_dist=""
+	if [ -r /etc/os-release ]; then
+		lsb_dist="$(. /etc/os-release && echo "$ID")"
+	fi
+echo "$lsb_dist"
 
 printf "\\n\\n    ${CY}Press ${WT}any ${CY}key to return to the Main Easy Linux menu."
   read -r -n1 -s -t 90
