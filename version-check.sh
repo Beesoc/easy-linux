9258eebee1a8061a1d7c1e82cae6529a4981c4e5
beesoc <beesoc@hotmail.com>
2023-04-30 17:31:27 -0500

    version check file

--- version-check.sh
+++ version-check.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
-
+set -e
+#shellcheck source=.envrc
+source .envrc
+scripts_dir=/opt/easy-linux
 # Define the minimum required version for each script
 declare -A required_versions=(
-  ["menu-master.sh"]="0.0.2"
-  ["menu-hacking.sh"]="0.0.2"
-  ["menu-customize.sh"]="0.0.2"
-  ["menu-apps.sh"]="0.0.2"
-  ["menu-backup_pwn-script.sh"]="0.0.2"
-  ["menu-upload-hashes.sh"]="0.0.2"
+  ["$scripts_dir/menu-master.sh"]="0.0.2"
+  ["$scripts_dir/menu-hacking.sh"]="0.0.2"
+  ["$scripts_dir/menu-customize.sh"]="0.0.2"
+  ["$scripts_dir/menu-apps.sh"]="0.0.2"
+  ["$scripts_dir/menu-backup_pwn-script.sh"]="0.0.2"
+  ["$scripts_dir/.envrc"]="0.0.2"
+  ["$scripts_dir/README.md"]="0.0.2"
+  ["$scripts_dir/version-check.sh"]="0.0.2"
+  ["$scripts_dir/SETUP.sh"]="0.0.2"
+  ["$scripts_dir/INSTALL.sh"]="0.0.2"
+  ["$scripts_dir/INSTALL.zip"]="0.0.2"
 
+  ["$scripts_dir/support/Banner_func.sh"]="0.0.2"
+  ["$scripts_dir/support/support-fatrat.sh"]="0.0.2"
+  ["$scripts_dir/support/support-fix-my-perm.sh"]="0.0.2"
+  ["$scripts_dir/support/support-hxcdump.sh"]="0.0.2"
+  ["$scripts_dir/support/support-linux_connection_script.sh"]="0.0.2"
+  ["$scripts_dir/support/support-makeWordlist.sh"]="0.0.2"
+  ["$scripts_dir/support/support-Prompt_func.sh"]="0.0.2"
+  ["$scripts_dir/support/support-sysinfo.sh"]="0.0.2"
+  ["$scripts_dir/support/support-trap-wifi.sh"]="0.0.2"
+  ["$scripts_dir/support/support-webmin.sh"]="0.0.2"
+  ["$scripts_dir/support/support-wifite.sh"]="0.0.2"
+
+  # add more files from support subdirectory as needed
 )
+
 
 # Loop through each script and check its version against the minimum required version
 for script in "${!required_versions[@]}"
@@ -22,7 +44,7 @@ do
   
   # Compare the current version to the required version
   if [[ "$(printf '%s\n' "$required_version" "$current_version" | sort -V | head -n1)" != "$required_version" ]]; then
-    echo "Error: $script version $required_version or higher is required (current version is $current_version)"
+    printf "${RED}    Error: ${WT}$script ${RED}version ${WT}$required_version ${RED}or higher is required (current version is ${RED}$current_version)"
     exit 1
   fi
 done
