4c3ae9b479104960dd52dbdc88d210dd88985888
beesoc <beesoc@hotmail.com>
2023-05-01 09:17:47 -0500

    remade zip files, deleted program files.

--- archive/support-wpaUP.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-sudo ifconfig wlan0 down
-sudo systemctl start NetworkManager
-sudo systemctl start wpa_supplicant
-sudo systemctl start NetworkManager
-sudo systemctl start wpa_supplicant
-sudo macchanger -p wlan0
-sudo ifconfig wlan0 up
