46cd289ade69cbeb31a6612b7f470ad97ed841a0
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-03 07:57:47 -0500

    Update SETUP.sh

--- SETUP.sh
+++ SETUP.sh
@@ -5,10 +5,13 @@
 # Version: 0.0.2
 set -e
 
+scripts_dir=/opt/easy-linux
+compiled_dir=$HOME/compiled
+
 echo "#!/bin/bash
 # Cleanup script
 # Version: 0.0.2
-set -e
+# set -e
 
 scripts_dir=/opt/easy-linux
 compiled_dir=$HOME/compiled
@@ -16,14 +19,14 @@ compiled_dir=$HOME/compiled
 if [[ ! -d ${scripts_dir} ]]; then  
     return
 elif [[ -d ${scripts_dir} ]]; then 
-    sudo rm -RF ${scripts_dir}
+    sudo rm -Rf ${scripts_dir}
 else 
     return
 fi
 if [[ ! -d ${compiled_dir}/easy-linux ]]; then  
     return
 elif [[ -d ${compiled_dir}/easy-linux ]]; then 
-    sudo rm -RF ${compiled_dir}/easy-linux
+    sudo rm -Rf ${compiled_dir}/easy-linux
 else 
     return
 fi" > .cleanup.sh
@@ -33,18 +36,16 @@ echo "#!/bin/bash
 sudo bash ./.cleanup.sh
 exit" > .cleanup2.sh
 
-cd ${compiled_dir}/easy-linux || exit
-direnv allow && sudo direnv allow
 sudo chmod a+x ./.cleanup2.sh
 
-
 trap ./.cleanup2.sh EXIT
+
+# trap ./.cleanup.sh EXIT
 CY='\e[1;36m'
 WT='\e[1;37m'
 RED='\e[1;31m'
 NC='\e[0m'
-scripts_dir=/opt/easy-linux
-compiled_dir=$HOME/compiled
+
 clear
 Banner_func() {
   printf "${WT}\\n
@@ -66,7 +67,11 @@ Banner_func() {
 #  █ ▌▀ ▄ ╚ ╝ ╔ ╗ ═ ║  Characters used in the banner.
 }
 Banner_func
-printf "\\n${CY}                 Welcome to the Installer for Beesoc's Easy Linux.                   ${CY}${NC}\\n" 
+printf "\\n${WT}                 Welcome to the Installer for Beesoc's Easy Linux.                   ${CY}${NC}\\n" 
+printf "\\n\\n\\n         ${CY}This installer will create the necessary folders and then clone\\n"
+printf "        the official repo for ${WT}Beesoc's Easy Linux ${CY}for installation. You will \\n"
+printf "        need a ${WT}Github username ${CY}and ${WT}fine-grained access token ${CY}to continue.\\n"
+printf "${WT}        For more info on Github's Personal Access Token see https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token.\\n"
 printf "\\n${CY}Press ${WT}any ${CY}key to continue.                            Press ${RED}[ctrl+c] ${CY}to cancel\\n"
 #printf "${WT}\\n    ---->"
 printf "${WT} \\n"
@@ -92,5 +97,7 @@ printf "${WT} \\n"
     cd ${compiled_dir} || exit
     git clone https://github.com/Beesoc/easy-linux.git
     cd easy-linux || exit
+    sudo chmod a+x ./.cleanup.sh
+    sudo chmod a+x ./.cleanup2.sh
     sudo chmod a+x ./INSTALL.sh
     bash ./INSTALL.sh
