236e06472848741efc3df3d83a05bd1535e6fd2e
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-03 06:50:06 -0500

    finished updates for initial release

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,6 +7,9 @@
 # Version: 0.0.2
 set -e
 
+scripts_dir="/opt/easy-linux"
+compiled_dir=$HOME/compiled
+
 echo "#!/bin/bash
 # Cleanup script
 # Version: 0.0.2
@@ -36,8 +39,6 @@ CY='\e[1;36m'
 WT='\e[1;37m'
 
 #printf "${BG}"
-scripts_dir="/opt/easy-linux"
-compiled_dir=$HOME/compiled
 
 support-Banner_func() {
   printf "${WT}\\n
@@ -64,22 +65,22 @@ folder_exists_func() {
   support-Banner_func
 
 if [[ ! -d "${scripts_dir}" ]]; then  
-    printf "  ${CY}$scripts_dir not found.\\n${CY}    Please Wait, creating ${scripts_dir} dir."; sleep 1 
+    printf "  ${CY}${scripts_dir} not found.\\n${CY}    Please Wait, creating ${scripts_dir} dir."; sleep 1 
     printf "${WT}.."; sleep 1; printf ".."; sleep 1
-    sudo mkdir "${scripts_dir}"
-elif [[ -d "${scripts_dir}" ]]; then 
+    sudo mkdir ${scripts_dir}
+elif [[ -d ${scripts_dir} ]]; then 
     printf "${scripts_dir} found. Continuing."
 else 
     printf "     ${RED} Unknown error detected. Exiting."
     exit 1
 fi
-  sudo chown -Rf 1000:0 $scripts_dir
+  sudo chown -Rf 1000:0 ${scripts_dir}
 
-  if [[ ! -d "${scripts_dir}/tmp" ]]; then  
-        printf "  ${CY}${scripts_dir}/tmp not found.\\n${CY}    Please Wait, creating ${scripts_dir} dir."; sleep 1 
+  if [[ ! -d ${scripts_dir}/tmp ]]; then  
+        printf "  ${CY}${scripts_dir}/tmp not found.\\n${CY}    Please Wait, creating ${scripts_dir} directory."; sleep 1 
         printf "${WT}.."; sleep 1; printf ".."; sleep 1
-        sudo mkdir "${scripts_dir}/tmp"
-  elif [[ -d "${scripts_dir}/tmp" ]]; then 
+        sudo mkdir ${scripts_dir}/tmp
+  elif [[ -d ${scripts_dir}/tmp ]]; then 
       printf "${scripts_dir}/tmp found. Continuing."
       sudo chown -Rf 1000:0 ${scripts_dir}/tmp
       sudo rm -Rf ${scripts_dir}/tmp/
@@ -89,11 +90,11 @@ fi
        exit 1
   fi
 
-if [[ ! -d "${scripts_dir}/support" ]]; then  
-        printf "  ${CY}${scripts_dir}/support not found.\\n${CY}    Please Wait, creating ${scripts_dir}/support dir."; sleep 1 
+if [[ ! -d ${scripts_dir}/support ]]; then  
+        printf "  ${CY}${scripts_dir}/support not found.\\n${CY}    Please Wait, creating ${scripts_dir}/support directory."; sleep 1 
         printf "${WT}.."; sleep 1; printf ".."; sleep 1
-        sudo mkdir "${scripts_dir}/support"
-  elif [[ -d "${scripts_dir}/support" ]]; then 
+        sudo mkdir ${scripts_dir}/support
+  elif [[ -d ${scripts_dir}/support ]]; then 
       printf "${scripts_dir}/support found. Continuing."
       sudo chown -Rf 1000:0 ${scripts_dir}/support
       sudo rm -Rf ${scripts_dir}/support/
@@ -102,11 +103,11 @@ if [[ ! -d "${scripts_dir}/support" ]]; then
        printf "     ${RED} Unknown error detected. Exiting."
        exit 1
   fi  
-  if [[ ! -d "${scripts_dir}/install" ]]; then  
-        printf "  ${CY}${scripts_dir}/install not found.\\n${CY}    Please Wait, creating ${scripts_dir}/install dir."; sleep 1 
+  if [[ ! -d ${scripts_dir}/install ]]; then  
+        printf "  ${CY}${scripts_dir}/install not found.\\n${CY}    Please Wait, creating ${scripts_dir}/install directory."; sleep 1 
         printf "${WT}.."; sleep 1; printf ".."; sleep 1
         sudo mkdir "${scripts_dir}/install"
-  elif [[ -d "${scripts_dir}/install" ]]; then 
+  elif [[ -d ${scripts_dir}/install ]]; then 
       printf "${scripts_dir}/install found. Continuing."
       sudo chown -Rf 1000:0 ${scripts_dir}/install
       sudo rm -Rf ${scripts_dir}/install/
@@ -126,34 +127,34 @@ clear
     support-Banner_func
     sudo apt install -y bc zip direnv lm-sensors >/dev/null
           printf " ${WT} "
-    read -pr "   Should I install to default location? [Y/N]  " install
+    read -r "   Should I install to default location? [Y/N]  " install
         if [[ ${install} == "n" ]] || [[ ${install} == "N" ]]; then
             printf "${RED}     You chose not to install. Quiting application"
             exit 1
         elif [[ ${install} == "y" ]] || [[ ${install} == "Y" ]]; then
           printf "${CY}Unzipping files into ${WT}'${scripts_dir}/tmp' ${CY}and then installing to ${WT}${scripts_dir}\\n"
-          sudo mv -f ${compiled_dir}*.zip $scripts_dir/tmp/ 
+          sudo mv -f ${compiled_dir}* ${scripts_dir}/tmp/ 
+          sudo cp ${compiled_dir}/.envrc ${scripts_dir}
+            cd ${scripts_dir} && direnv allow && sudo direnv allow
+          sudo cp ${compiled_dir}/.envrc ${scripts_dir}/support
+            cd ${scripts_dir}/support && direnv allow && sudo direnv allow
+          sudo cp ${compiled_dir}/.envrc ${scripts_dir}/install
+            cd ${scripts_dir}/install && direnv allow && sudo direnv allow
+          sudo cp ${compiled_dir}/.shellcheckrc ${scripts_dir}/ 
+          
           cd ${scripts_dir}/tmp || exit
           sudo unzip -uqo *.zip
           sudo cp -rf ./*.sh ${scripts_dir}
           
-          sudo mv $scripts_dir/tmp/support/* ${scripts_dir}/support
+          sudo mv ${scripts_dir}/tmp/support/* ${scripts_dir}/support
           sudo mv ${scripts_dir}/tmp/install/* ${scripts_dir}
-          sudo cp $compiled_dir/* $scripts_dir
-          sudo cp ${compiled_dir}/.envrc $scripts_dir
-            cd ${scripts_dir} || exit && direnv allow && sudo direnv allow
-          sudo cp ${compiled_dir}/.envrc $scripts_dir/support
-            cd ${scripts_dir}/support || exit && direnv allow && sudo direnv allow
-          sudo cp ${compiled_dir}/.envrc ${scripts_dir}/install
-            cd ${scripts_dir}/install || exit && direnv allow && sudo direnv allow
-          sudo cp ${compiled_dir}/.shellcheckrc ${scripts_dir}/ 
-          sudo chown -vR 1000:0 ${scripts_dir}
-          sudo chmod -R a+x ${scripts_dir}
+          sudo chown -vR 1000:0 ${scripts_dir}  
+          sudo chmod -R a+x ${scripts_dir}*.sh
             sudo cp ${scripts_dir}/install/menu-master.sh /usr/bin
             sudo mv install/easy-linux.desktop /usr/share/applications/easy-linux.desktop
-            sudo cp .envrc $scripts_dir && direnv allow && sudo direnv allow
-        sudo touch ${scripts_dir}support/adapter
-        cd $scripts_dir || exit && direnv allow && sudo direnv allow
+            sudo cp .envrc ${scripts_dir} && direnv allow && sudo direnv allow
+        sudo touch ${scripts_dir}/support/adapter
+        cd ${scripts_dir} && direnv allow && sudo direnv allow
         else
             printf "     Invalid selection\\n"
         fi
@@ -162,10 +163,17 @@ clear
 cleanup_func() {
   printf " - Please Wait while I cleanup some files used in the installation - \\n" 
   printf "${WT}..."; slept 1; printf "...Almost done\\n" 
-  sudo rm -f ${scripts_dir}tmp/*.*
-  sudo rmdir -f tmp/
-  rm -f ${compiled_dir}/beesoc-menu/*
-  sudo rmdir -f ${compiled_dir}/beesoc-menu/
+  
+if [[ -d ${scripts_dir}/easy-linux/tmp ]]; then
+  sudo rm -Rf ${scripts_dir}tmp/
+fi
+if [[ -d ${compiled_dir}/beesoc-menu ]]; then
+  rm -Rf ${compiled_dir}/beesoc-menu/
+fi
+if [[ -d ${compiled_dir}/easy-linux ]]; then
+  rm -Rf ${compiled_dir}/easy-linux/*
+fi
+
   printf "...done.${CY}"
   clear
   support-Banner_func
