610ab2d9e9800d38181719e06e53c7ed520cf126
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-03 08:22:20 -0500

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -22,14 +22,14 @@ cleanup_func() {
 if [[ ! -d ${scripts_dir} ]]; then  
     return
 elif [[ -d ${scripts_dir} ]]; then 
-    sudo rm -RF ${scripts_dir}
+    sudo rm -Rf ${scripts_dir}
 else 
     return
 fi
 if [[ ! -d ${compiled_dir}/easy-linux ]]; then  
     return
 elif [[ -d ${compiled_dir}/easy-linux ]]; then 
-    sudo rm -RF ${compiled_dir}/easy-linux
+    sudo rm -Rf ${compiled_dir}/easy-linux
 else 
     return
 fi
@@ -89,7 +89,7 @@ fi
   sudo chown -Rf 1000:0 ${scripts_dir}
 
   if [[ ! -d ${scripts_dir}/tmp ]]; then  
-        printf "  ${CY}${scripts_dir}/tmp not found.\\n${CY}    Please Wait, creating ${scripts_dir} directory."; sleep 1 
+        printf "  ${CY}${scripts_dir}/tmp not found.\\n${CY}    Please Wait, creating ${scripts_dir}/tmp directory."; sleep 1 
         printf "${WT}.."; sleep 1; printf "..\\n"; sleep 1
         sudo mkdir ${scripts_dir}/tmp
   elif [[ -d ${scripts_dir}/tmp ]]; then 
@@ -138,8 +138,8 @@ install_func() {
 clear
     support-Banner_func
     sudo apt install -y bc zip direnv lm-sensors >/dev/null
-    printf "  ${OG}Press ${WT}any ${OG} key to install Beesoc's Easy Linux Loader...or cancel with ${RED}Ctrl C${OG}"
-    printf " ${WT}    ----->"
+    printf "  ${CY}Press ${WT}any ${CY} key to install ${WT}Beesoc's Easy Linux Loader...or cancel with ${RED}Ctrl C${WT}\\n"
+#    printf " ${WT}    ----->"
     read -r -n1 -s -t 60
 #    read -r "Should I install to default location? [Y/N]  " install
 #        if [[ ${install} == "n" ]] || [[ ${install} == "N" ]]; then
@@ -148,24 +148,24 @@ clear
 #        elif [[ ${install} == "y" ]] || [[ ${install} == "Y" ]]; then
           printf "${CY}Unzipping files into ${WT}'${scripts_dir}/tmp' ${CY}and then installing to ${WT}${scripts_dir}\\n"
           sudo cp -Rf ${compiled_dir}/easy-linux/* ${scripts_dir}/tmp/ 
-          sudo cp ${compiled_dir}/easy-linux/.envrc ${scripts_dir}
+          sudo cp -Rf ${compiled_dir}/easy-linux/.envrc ${scripts_dir}
             cd ${scripts_dir} && direnv allow && sudo direnv allow
-          sudo cp ${compiled_dir}/easy-linux/.envrc ${scripts_dir}/support
+          sudo cp -Rf ${compiled_dir}/easy-linux/.envrc ${scripts_dir}/support
             cd ${scripts_dir}/support && direnv allow && sudo direnv allow
-          sudo cp ${compiled_dir}/easy-linux/.envrc ${scripts_dir}/install
+          sudo cp -Rf ${compiled_dir}/easy-linux/.envrc ${scripts_dir}/install
             cd ${scripts_dir}/install && direnv allow && sudo direnv allow
-          sudo cp ${compiled_dir}/easy-linux/.shellcheckrc ${scripts_dir}/ 
+          sudo cp -Rf ${compiled_dir}/easy-linux/.shellcheckrc ${scripts_dir}/ 
           
           cd ${scripts_dir}/tmp || exit
           sudo unzip -uqo *.zip
-          sudo cp -rf ./*.sh ${scripts_dir}
+          sudo cp -Rf ./*.sh ${scripts_dir}
           
           sudo mv ${scripts_dir}/tmp/support/* ${scripts_dir}/support
           sudo mv ${scripts_dir}/tmp/install/* ${scripts_dir}
           sudo chown -vR 1000:0 ${scripts_dir}  
           sudo chmod -R a+x ${scripts_dir}*.sh
-            sudo cp -f ${scripts_dir}/menu-master.sh /usr/bin
-            sudo cp -f easy-linux.desktop /usr/share/applications/easy-linux.desktop
+            sudo cp -Rf ${scripts_dir}/menu-master.sh /usr/bin
+            sudo cp -Rf easy-linux.desktop /usr/share/applications/easy-linux.desktop
             sudo touch ${scripts_dir}/support/adapter
         cd ${scripts_dir} && direnv allow && sudo direnv allow
 }
@@ -185,6 +185,7 @@ if [[ -d ${compiled_dir}/easy-linux ]]; then
 fi
   sleep 1
   printf "...done.${CY}"
+  
   clear
   support-Banner_func
   printf "   ${CY}Beesoc's Easy Linux Loader has been installed.\\n\\n" 
