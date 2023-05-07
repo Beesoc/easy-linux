f12063dd7061f8b001390ebb1f09b5fcad525d24
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-07 12:48:34 -0500

    Add files via upload

---
+++ INSTALLv2.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+# New app installer from Github
+# Version: 0.0.2
+sudo chown -vR 1000:1000 .
+sudo chmod +x *.sh
+sudo chmod +x install/*.sh
+sudo chmod +x support/*.sh
+sudo cp -fr install/*.sh /opt/easy-linux
+sudo cp -f install/easy-linux.desktop /usr/share/applications/
