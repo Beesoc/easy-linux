c896bfb857ed60d6e2bcc434766a07e535fcce38
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-07 13:24:31 -0500

    Update INSTALLv2.sh

--- INSTALLv2.sh
+++ INSTALLv2.sh
@@ -2,7 +2,7 @@
 # New app installer from Github
 # Version: 0.0.2
 sudo git clone https://github.com/Beesoc/easy-linux.git /opt/easy-linux && cd /opt/easy-linux
-sudo chown -vR 1000:1000 .
+sudo chown -vR 1000:1000 /opt/easy-linux
 sudo chmod +x *.sh
 sudo chmod +x install/*.sh
 sudo chmod +x support/*.sh
