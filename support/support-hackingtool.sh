2c7c53646201782e0e67afcf8eccf75bd12c93bb
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-08 15:48:55 -0500

    Create support-hackingtool.sh

---
+++ support/support-hackingtool.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+scripts_dir=/opt/easy-linux
+#
+# Version: 0.0.2
+#shellcheck source=${scripts_dir}/.envrc
+#shellcheck source="${scripts_dir}/support/support-Prompt_func.sh"
+#shellcheck source="${scripts_dir}/support/support-Banner_func.sh"
+set -e
+#
+clear
+#
+source ${scripts_dir}/.envrc
+
+  if [ ! -d ${scripts_dir} ]; then
+        printf "${RED}   ERROR: ${scripts_dir} is not found.  Please reinstall Easy Linux${NC}"
+  fi
+
+
+printf "${CY}Installing ${WT}Hacking Tool${CY}...${NC}\n"
+  if [[ -d ${compiled_dir} ]]; then
+        printf " \\n";
+          cd ~/compiled || exit
+          return 1
+  elif [[ ! -d ${compiled_dir} ]]; then
+          mkdir ~/compiled 
+          cd ~/compiled || exit
+  else
+       printf "${RED}Invalid Selection"
+  fi
+  
+  
+  sudo git clone https://github.com/Z4nzu/hackingtool.git
+  cd hackingtool || exit
+  sudo pip3 install -r requirements.txt
+  bash ./install.sh
+}
