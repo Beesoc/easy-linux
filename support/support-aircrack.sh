ef2b48d877df92cd966c40c1812e8482eccaa030
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-10 07:01:30 -0500

    Add files via upload

--- support/support-aircrack.sh
+++ support/support-aircrack.sh
@@ -2,6 +2,10 @@
 # Aircrack-ng installer/executor
 # Version: 0.0.2
 
+scripts_dir=/opt/easy-linux
+source "${scripts_dir}/support/support-Banner_func.sh"
+source "${scripts_dir}/.envrc"
+
 if [[ $(cat ${scripts_dir}/.envrc | grep "depsinstalled" -c) = 0 ]]; then
     depsinstalled=0
 fi
@@ -10,46 +14,42 @@ if [[ $(cat ${scripts_dir}/.envrc | grep "airinstalled" -c) = 0 ]]; then
     airinstalled=0
 fi
 
-set -e
-scripts_dir=/opt/easy-linux
-source "${scripts_dir}/.envrc"
-source "${scripts_dir}/support/support-Banner_func.sh"
-
 check_directories_func() {
-# Step 2 or 4 function.
-if [[ $airinstalled = 1 ]]; then 
-    run_func
-else
-    printf "${WT}  Aircrack-ng ${CY}not detected. Continuing...check_directories_func"
-fi
+    # Step 2 or 4 function.
+    if [[ $airinstalled = 1 ]]; then 
+        run_func
+    else
+        printf "${WT}  Aircrack-ng ${CY}not detected. Continuing...\\n"
+    fi
 
-if [[ -d /opt/aircrack-ng ]]; then
-    printf "  ${WT}[*] ${GN}/opt/aircrack-ng ${CY}directory found.\\n"
-    printf "  ${WT}[*] ${GN}Removing and reinstalling." 
-    sleep 1
-    sudo rm -fr /opt/aircrack-ng
-elif [[ ! -d /opt/aircrack-ng ]]; then
-#    sudo chown -v 1000:1000 /opt
-    printf " ${WT} [*] ${WT}/opt/aircrack-ng ${CY}directory not found. Installing into that folder.\\n"; sleep 1
-fi  
-#curl https://download.aircrack-ng.org/aircrack-ng-1.7.tar.gz $HOME/Downloads
-sudo wget https://download.aircrack-ng.org/aircrack-ng-1.7.tar.gz -P $HOME/Downloads
+    if [[ -d /opt/aircrack-ng ]]; then
+        printf "  ${WT}[*] ${GN}/opt/aircrack-ng ${CY}directory found.\\n"
+        printf "  ${WT}[*] ${GN}Removing and reinstalling.\\n " 
+        sleep 1
+        sudo rm -fr /opt/aircrack-ng
+    elif [[ ! -d /opt/aircrack-ng ]]; then
+        #sudo chown -v 1000:1000 /opt
+        printf " ${WT} [*] ${WT}/opt/aircrack-ng ${CY}directory not found. Installing into that folder.\\n"; sleep 1
+    fi  
+    #curl https://download.aircrack-ng.org/aircrack-ng-1.7.tar.gz $HOME/Downloads
+    sudo wget https://download.aircrack-ng.org/aircrack-ng-1.7.tar.gz -P $HOME/Downloads
 
-app-install_func
+    app-install_func
 }
 
 run_func() {
-           depsinstalled=1
-     if [[ $(cat ${scripts_dir}/.envrc | grep "depsinstalled" -c) -gt 0 ]]; then
-          printf "${CY}...Variables previously written...Continuing...\\n"
-     elif [[ $(cat ${scripts_dir}/.envrc | grep "airinstalled" -c) != 1 ]]; then
-           sudo sh -c echo "export depsinstalled=$depsinstalled" >> ${scripts_dir}/.envrc
-           cd ${scripts_dir}/support && direnv allow
-     fi                 
-           airinstalled=1
-     if [[ $(cat ${scripts_dir}/.envrc | grep "airinstalled" -c) -gt 0 ]]; then
-           printf "${CY}...Variables previously written...Continuing...\\n"
-     elif [[ $(cat ${scripts_dir}/.envrc | grep "airinstalled" -c) != 1 ]]; then
+    depsinstalled=1
+    if [[ $(cat ${scripts_dir}/.envrc | grep "depsinstalled" -c) -gt 0 ]]; then
+        printf "${CY}...Variables previously written...Continuing...\\n"
+    elif [[ $(cat ${scripts_dir}/.envrc | grep "airinstalled" -c) != 1 ]]; then
+        sudo sh -c "echo 'export depsinstalled=$depsinstalled' >> ${scripts_dir}/.envrc"
+        cd ${scripts_dir}/support && direnv allow
+    fi                 
+    airinstalled=1
+    if [[ $(cat ${scripts_dir}/.envrc | grep "airinstalled" -c) -gt 0 ]]; then
+        printf "${CY}...Variables previously written...Continuing...\\n"
+    elif [[ $(cat ${scripts_dir}/.envrc | grep "airinstalled" -c) != 
+
            sudo sh -c echo "export airinstalled=$airinstalled" >> ${scripts_dir}/.envrc
            cd ${scripts_dir} && direnv allow
      fi
@@ -64,25 +64,25 @@ app-install_func() {
 sudo tar -xvf $HOME/Downloads/aircrack-ng-1.7.tar.gz
 
 if [[ $airinstalled = 1 ]]; then
-    printf "${GB}  Aircrack-ng already installed"
-    read -n 1 -s -r -p "Press any key to continue...app-install_func"
+    printf "${GB}  Aircrack-ng already installed\\n"
+    read -n 1 -s -r -p "Press any key to continue..."
     run_func
 else
   printf "${GN}  Loading, please wait."
   if [[ $depsinstalled = 1 ]]; then
-       printf "${OG} Dependencies already installed"
+       printf "${OG} Dependencies already installed\\n"
   elif [[ $depsinstalled != 1 ]]; then 
-   printf "${GN}"
+   printf "${GN}\\n"
        read -n 1 -p "Would you like to download and install Aircrack-ng dependancies? [Y/n] " installdeps
            installdeps=${installdeps:-Y}
            if [[ $installdeps =~ ^[Yy]$ ]]; then
-                   printf "${CY}  Downloading and installing dependencies.  ${WT}Please wait."
+                   printf "${CY}  Downloading and installing dependencies.  ${WT}Please wait.\\n"
                 sudo apt-get update
         sudo apt-get install -y build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev libssl-dev ethtool shtool rfkill zlib1g-dev libpcap-dev libsqlite3-dev libpcre3-dev libhwloc-dev libcmocka-dev hostapd wpasupplicant tcpdump screen iw usbutils
                 depsinstalled=1
                 sudo sh -c echo "export depsinstalled=$depsinstalled" >> ${scripts_dir}/.envrc
                 cd ${scripts_dir} && direnv allow
-                   printf "  ${GN}Dependencies installed successfully."
+                   printf "  ${GN}Dependencies installed successfully.\\n"
                            read -n 1 -p "Would you like to download and install Aircrack-ng? [Y/n] " installair
                            if [[ $installair =~ ^[Yy]$ ]]; then
                            sudo apt install -y hwloc
@@ -102,7 +102,7 @@ else
                            sudo make install
                            airinstalled=1  
                            elif [[ $installair = "N" ]] || [[ $installair = "n" ]]; then
-                           printf "${RED}  Not installing aircrack-ng.  Exiting."
+                           printf "${RED}  Not installing aircrack-ng.  Exiting.\\n"
                              exit 0
                            fi
                            sudo sh -c echo "export airinstalled=$airinstalled" >> ${scripts_dir}/.envrc
