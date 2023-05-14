c38c0cbe621e164faec9dbd8c2c9f31fa39c5b95
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-13 19:00:09 -0500

    Update .whoami.sh

--- support/.whoami.sh
+++ support/.whoami.sh
@@ -50,22 +50,6 @@ touch "$FLAG_FILE"
 fi
 
 # Verify the user and computer name in three different ways
-VERIFY_HOST=$(hostname)
-VERIFY_ETC_HOSTNAME=$(cat /etc/hostname)
-
-# Verify computer name using three different methods
-if [ "$computername" != "$(echo $HOST)" ]; then
-  printf "${OG}  Warning: $computername does not match $HOST" >&2
-fi
-if [ "$computername" != "$(cat /etc/hostname)" ]; then
-  printf "${OG}  Warning: $computername does not match $(cat /etc/hostname)" >&2
-fi
-if [ "$computername" != "$(uname -n)" ]; then
-  printf "${OG}  Warning: $computername does not match $(uname -n)" >&2
-fi
-if [ "$username" != "$userid" ] && [ "$userid" != "$useraccount" ]; then
-  printf "${OG}  Warning: $username does not match $USER" >&2
-fi
 
 cd /opt/easy-linux
 direnv allow
