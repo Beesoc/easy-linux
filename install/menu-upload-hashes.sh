768d917f5418723dd91d8da8efa45fff00ab7d9c
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-14 12:41:08 -0500

    Update menu-upload-hashes.sh

--- install/menu-upload-hashes.sh
+++ install/menu-upload-hashes.sh
@@ -10,50 +10,6 @@ source ${scripts_dir}/.envrc
 trap ${scripts_dir}/support/support-trap-wifi.sh EXIT
 source ${scripts_dir}/support/support-Banner_func.sh
 
-printf "  ${CY}This script backs up your saved hashes from ${WT}/opt/backup/root/handshakes${CY}."
-
-permissions_func() {
-    # make sure permissions are ok.
-    if [[ $HOST == "updates" ]]; then
-        pwnagotchi2=Gotcha
-        line=$(grep -n "pwnagotchi=" .envrc | cut -d: -f1)
-        sed -i "${line}s/.*/pwnagotchi=${pwnagotchi2}/" .envrc
-        
-        original_user2=beesoc
-        line=$(grep -n "ORIGINAL_USER=" .envrc | cut -d: -f1)
-        sed -i "${line}s/.*/original_user=${original_user2}/" .envrc
-        
-        sudo chown -vR beesoc:beesoc /opt/backup/root/*
-        elif [[ $HOST == "lepotato" ]]; then
-        pwnagotchi2=Sniffer
-        line=$(grep -n "pwnagotchi=" .envrc | cut -d: -f1)
-        sed -i "${line}s/.*/pwnagotchi=${pwnagotchi2}/" .envrc
-        
-        original_user2=larry
-        line=$(grep -n "ORIGINAL_USER=" .envrc | cut -d: -f1)
-        sed -i "${line}s/.*/original_user=${original_user2}/" .envrc
-        
-        sudo chown -vR larry:larry /opt/backup/root/*
-        
-    else
-        sudo chown -vR root:root /opt/backup/root/*
-    fi
-}
-
-folders_exist_func() {
-    # make sure /opt/backup/root exists
-    if [[ ! -d /opt/backup/ ]]; then
-        sudo mkdir /opt/backup
-    fi
-    if [[ ! -d /opt/backup/root/ ]]; then
-        sudo mkdir /opt/backup/root/
-    fi
-    if [[ ! -d "${handshakes_dir}" ]]; then
-        sudo mkdir /opt/backup/root/handshakes
-    fi
-}
-
-##
 upload_func() {
     printf " ${GN} [X] ${CY}Gzip is not supported on ${WT}OnlineHashCrack.\\n ${GN} [X] ${CY}Not compressing files. \\n ${GN} [*] ${CY}Uploading pcap's to OHC.${OG}\\n"
     wlancap2wpasec -u https://api.onlinehashcrack.com  -e enc.mail.prot@protonmail.com /opt/backup/root/handshakes/*.pcap
@@ -74,17 +30,58 @@ upload_func() {
     printf " ${GN} [X] ${CY}HCCAPX isn't supported on WPA-Stanev.\\n"
     clear
 }
+
+permissions_func() {
+    # make sure permissions are ok.
+    if [[ $HOST -eq $cwb_computername ]] && [[ $USER -eq $cwb_username]]; then
+        pwnagotchi=Gotcha
+        line=$(grep -n "pwnagotchi=" .envrc | cut -d: -f1)
+        sed -i "${line}s/.*/pwnagotchi=${pwnagotchi}/" .envrc
+        
+        sudo chown -vR beesoc:beesoc /opt/backup/root/*
+        elif [[ $HOST == "larry-linux" ]]; then
+        pwnagotchi2=Sniffer
+        line=$(grep -n "pwnagotchi=" .envrc | cut -d: -f1)
+        sed -i "${line}s/.*/pwnagotchi=${pwnagotchi2}/" .envrc
+        
+        original_user2=larry
+        line=$(grep -n "ORIGINAL_USER=" .envrc | cut -d: -f1)
+        sed -i "${line}s/.*/original_user=${original_user2}/" .envrc
+        
+        sudo chown -vR larry:larry /opt/backup/root/*
+        
+    else
+        sudo chown -vR root:root /opt/backup/root/*
+    fi
+upload_func
+}
+
+folders_exist_func() {
+    # make sure /opt/backup/root exists
+    if [[ ! -d /opt/backup/ ]]; then
+        sudo mkdir /opt/backup
+    fi
+    if [[ ! -d /opt/backup/root/ ]]; then
+        sudo mkdir /opt/backup/root/
+    fi
+    if [[ ! -d "${handshakes_dir}" ]]; then
+        sudo mkdir /opt/backup/root/handshakes
+    fi
+permissions_func
+}
+
 main() {
     clear
     source "${scripts_dir}/support/support-Banner_func.sh"
+    printf "  ${CY}This script backs up your saved hashes from ${WT}$handshakes${CY}.\\n"
+
     folders_exist_func
-    permissions_func
-    upload_func
 }
+
 main
 clear
 printf "${CY}All Operations completed. Cracked passwords will start appearing on \\n"
 printf "https://www.OnlineHashCrack.com and https://wpa-sec.stanev.org within 24hr.\\n"
 printf "Press ${WT}any${CY} key to return to ${WT}Main Menu.\\n "
-read -r -n1 -s -t 30
-bash ${scripts_dir}/menu-master.sh
+read -n 1 -r -s -t 120
+source ${scripts_dir}/menu-master.sh
