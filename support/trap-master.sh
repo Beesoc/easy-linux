7a4b3c7c51553ca8d5ab9e169be833c180854ce5
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-15 14:34:53 -0500

    Create trap-master.sh

---
+++ support/trap-master.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Trap Master
+# Version: 0.0.2
+scripts_dir=/opt/easy-linux
+source ${scripts_dir}/.envrc
+printf "${GN}*********************************** Exiting ************************************"
