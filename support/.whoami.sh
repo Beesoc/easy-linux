531583b31bc15543c922cecfe9baeba8b32c13b3
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-14 23:39:39 -0500

    Update .whoami.sh

--- support/.whoami.sh
+++ support/.whoami.sh
@@ -73,6 +73,19 @@ pkg_info_func() {
 
 sudo dpkg --get-selections > ${scripts_dir}/support/my_installed_apps.list
 cat ${scripts_dir}/support/my_installed_apps.list | grep "install" -c
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+else
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+
 }
 function coloredEcho(){
     local exp=$1;
