4f22ffcf6c3b839643aaac591ec4e80dbbd5171a
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-15 00:22:26 -0500

    Update .whoami.sh

--- support/.whoami.sh
+++ support/.whoami.sh
@@ -83,9 +83,6 @@ if [ -f /etc/os-release ]; then
     . /etc/os-release
     OS=$NAME
     VER=$VERSION_ID
-elif type lsb_release >/dev/null 2>&1; then
-    OS=$(lsb_release -si)
-    VER=$(lsb_release -sr)
 else
     OS=$(uname -s)
     VER=$(uname -r)
