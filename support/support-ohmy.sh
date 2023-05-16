b17e6af4cdcfa5edd38d04cad9a02ce7d7b160a7
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-15 19:11:35 -0500

    Update support-ohmy.sh

--- support/support-ohmy.sh
+++ support/support-ohmy.sh
@@ -2,60 +2,62 @@
 #Version: 0.0.2
 scripts_dir=/opt/easy-linux
 source ${scripts_dir}/.envrc
-                        my_shell=$(echo $SHELL)
+                       
+zsh_install () {
+printf "ZSH was not found. ${CY}Press ${WT}any ${CY}key to continue.\\n\\n"
+read -n 1 -t 300
+sudo apt install -y zsh      
+}                       
+                   
+                        
+ default_shell_func() {
+           read -p "What do you want as your default shell, [Z]SH or [B]ASH? [Z/b] " default_shell
+
+           default_shell=$(default_shell:-Z)
+                   if [[ "$default_shell" =~ ^[Zz] ]]; then
+                   sudo chsh -s $(which zsh)
+                   elif [[ "$default_shell" =~ ^[Bb] ]]; then
+                   sudo chsh -s $(which bash)
+                   else               
+                   printf "${CY}Invalid selection.\\n"
+                   exit 0
+                   fi           
+}
+
+main() {
+     my_shell=$(echo $SHELL)
                         printf "  ${CY}Here you can easily install the Oh My BASH and Oh My ZSH projects\\n"
                         printf "  to greatly enhance your terminal. I LOVE these projects.\\n"
                         printf "  ${CY}Your current shell is ${WT}$my_shell.\\n"
-                        printf "  ${CY}Do you want Oh My [B]ASH, or Oh My [Z]SH? [B/z] "
+                        default_shell_func
+                        printf "  ${CY}Do you want install Oh My BASH? [B/z] "
                         read -r -n 1 -s shellchoice
                         shellchoice=${shellchoice:-B}
-                        if [[ $shellchoice == "B" ]]; then
-                                if [[ $my_shell != "/usr/bin/bash" ]]; then
-                                read -p "BASH is not your default shell. Should I make it so? [Y/n] " bash_default
-                                bash_default=$(bash_default:-Y)
-                                        if [[ $bash_default =~ ^[Yy] ]]; then
-                                        sudo chsh -s $(which bash)
-                                        elif [[ $bash_default =~ ^[Nn] ]]; then
-                                        printf "${WT}$USER ${CY}has selected to NOT change the default shell.\\n"
-                                        fi
-                                elif [[ $my_shell = "/usr/bin/bash" ]]; then 
-                                        if [[ -d $HOME/.oh-my-bash ]]; then
-                                        printf "  ${CY}You have already installed Oh My BASH!\\n"
-                                        printf "  ${CY}Press ${WT}any ${CY}key to continue.\\n\\n"
-                                        read -n 1 -t 300
-                                        source ${scripts_dir}/menu-master.sh
-                                        elif  [[ ! -d $HOME/.oh-my-bash ]]; then
-                                               bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
-                                        fi
-                                 fi
-                         fi
-                     if [[ $shellchoice == "Z" ]]; then
-                          if [[ $(zsh --version > /dev/null  2>&1) ]]; then
-                                printf "  ${CY}ZSH not found. ${CY}Press ${WT}any ${CY}key to continue.\\n\\n"
-                                read -n 1 -t 300
-                                sudo apt install -y zsh
-                          else
-                             printf "  ZSH found.  Continuing. \\n"
-                          fi
-                          if [[ $my_shell != "/usr/bin/zsh" ]]; then
-                                read -p "ZSH is not your default shell. Should I make it so? [Y/n] " zsh_default
-                                zsh_default=$(zsh_default:-Y)
-                                        if [[ $zsh_default =~ ^[Yy] ]]; then
-                                        sudo chsh -s $(which zsh)
-                                        elif [[ $zsh_default =~ ^[Nn] ]]; then
-                                        printf "${WT}$USER ${CY}has selected to NOT change the default shell.\\n"
-                                        exit 0
-                                        fi
-                          elif [[ $my_shell = "/usr/bin/zsh" ]]; then 
-                                        if [[ -d $HOME/.oh-my-zsh ]]; then
-                                        printf "  ${CY}You have already installed Oh My ZSH!\\n"
-                                        printf "  ${CY}Press ${WT}any ${CY}key to continue.\\n\\n"
-                                        read -n 1 -t 300
-                                        source ${scripts_dir}/menu-master.sh
-                                        elif  [[ ! -d $HOME/.oh-my-zsh ]]; then
-                                        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-                                        source ${scripts_dir}/menu-master.sh
-                                        fi      
-                           fi
-                      fi        
-               
+                   if [[ $shellchoice == "B" ]]; then
+                               if  [[ ! -d $HOME/.oh-my-bash ]]; then
+                               bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
+                               elif  [[ -d $HOME/.oh-my-bash ]]; then
+                               printf "  Oh My BASH already installed."
+                               fi
+                   elif [[ $shellchoice == "Z" ]]; then
+                         if ! command -v zsh &> /dev/null; then
+                         zsh_install
+                         else
+                         printf "ZSH is already installed.  Continuing"
+                              if [[ -d $HOME/.oh-my-zsh ]]; then
+                                   printf "  ${CY}You have already installed Oh My ZSH!\\n"
+                                   printf "  ${CY}Press ${WT}any ${CY}key to continue.\\n\\n"
+                                   read -n 1 -t 300
+                                   source ${scripts_dir}/menu-master.sh
+                              elif [[ ! -d $HOME/.oh-my-zsh ]]; then
+                                   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+                                   source ${scripts_dir}/menu-master.sh
+                              fi              
+                          fi  
+                     else
+                            printf "  ${RED}Invalid Selection. \\n"
+                     exit 0
+                     fi
+   }
+   
+   main
