1b0cf4be213f697a2c4d4a42436cb53bc0f955e6
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-18 08:23:31 -0500

    Update .whoami.sh

--- support/.whoami.sh
+++ support/.whoami.sh
@@ -8,6 +8,15 @@ cd ${scripts_dir}
 direnv allow && sudo direnv allow
 source /opt/easy-linux/.envrc
 
+pkg_info_func() {
+    
+    sudo dpkg --get-selections > ${scripts_dir}/support/my_installed_apps.list
+    tot_pkgs=$(cat ${scripts_dir}/support/my_installed_apps.list | grep "install" -c)
+    
+    KERN=$(uname -r)
+    
+}
+
 get_vars_func() {
     computername=$(cat /etc/hostname)
     user=$USER
@@ -37,18 +46,18 @@ get_vars_func() {
             # Add more environment variables as needed
             #    echo "export MY_VAR=my_value" | /opt/easy-linux/.envrc
             echo "export docker_installed=0" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export fatrat_installed=0" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export hacktool_installed=0" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export hcxdump_installed=0" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export fatrat_installed=$(if [ $(command -v fatrat) ]; then echo "1"; else echo "0"; fi)" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export hacktool_installed=$(if [[ $(command -v hackingtool) ]]; then echo "1"; else echo "0"; fi)" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export hcxdump_installed=$(if [[ $(command -v hcxdumptool) ]]; then echo "1"; else echo "0"; fi)" | sudo tee -a ${scripts_dir}/.envrc
             echo "export nano_installed=0" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export webmin_installed=0" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export wifite_installed=0" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export webmin_installed=$(if [[ $(command -v webmin) ]]; then echo "1"; else echo "0"; fi)" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export wifite_installed=$(if [[ $(command -v wifite) ]]; then echo "1"; else echo "0"; fi)" | sudo tee -a ${scripts_dir}/.envrc
             echo "export stand_install=0" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export airc_installed=0" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export airc_deps_installed=0" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export airg_deps_inst=0" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export airg_installed=0" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export autoj_install=0" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export airc_installed=$(if [[ $(command -v aircrack-ng) ]]; then echo "1"; else echo "0"; fi)" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export airc_deps_installed=$(if [[ $(command -v aircrack-ng) ]]; then echo "1"; else echo "0"; fi)" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export airg_deps_inst=$(if [[ $(command -v airgeddon) ]]; then echo "1"; else echo "0"; fi)" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export airg_installed=$(if [[ $(command -v airgeddon) ]]; then echo "1"; else echo "0"; fi)" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export autoj_install=$(if [[ $(command -v autojump) ]]; then echo "1"; else echo "0"; fi)" | sudo tee -a ${scripts_dir}/.envrc
             echo "export user=$USER" | sudo tee -a ${scripts_dir}/.envrc
             echo "export username=$(whoami)" | sudo tee -a ${scripts_dir}/.envrc
             echo "export useraccount=$(getent passwd 1000 | cut -d ":" -f 1)" | sudo tee -a ${scripts_dir}/.envrc
@@ -57,13 +66,15 @@ get_vars_func() {
             echo "export computername=$(cat /etc/hostname)" | sudo tee -a ${scripts_dir}/.envrc
             echo "export arch=$(uname -m)" | sudo tee -a ${scripts_dir}/.envrc
             echo "export wordlist=/usr/share/wordlists/Top304Thousand-probable-v2.txt" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export amiPwn=$(if [ -f "/etc/pwnagotchi/config.toml" ]; then echo 1; else echo 0; fi)" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export pwnagotchi=Unknown" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export amiPwn=$(if [ -f "/etc/pwnagotchi/config.toml" ]; then echo "1"; else echo "0"; fi)" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export pwnagotchi=$(if [ $USER -eq $cwb_username && /opt/hostname -eq $cwb_updates ]; then echo "Gotcha"; else echo "Unknown"; fi)" | sudo tee -a ${scripts_dir}/.envrc
             echo "export pwn_installed=0" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export OS=$OS" | sudo tee -a ${scripts_dir}/.envrc
-            echo "export VER=$VER" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export OS=$(uname -s)" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export VER=$(uname -v)" | sudo tee -a ${scripts_dir}/.envrc
             echo "export KERN=$(uname -r)" | sudo tee -a ${scripts_dir}/.envrc
-        }
+            echo "export ALLINFO=$(uname -a)" | sudo tee -a ${scripts_dir}/.envrc
+            echo "export tot_pkgs=$(cat ${scripts_dir}/support/my_installed_apps.list | grep "install" -c) | sudo tee -a ${scripts_dir}/.envrc
+  }
         
         # Populate the envrc table
         populate_envrc
@@ -73,58 +84,18 @@ get_vars_func() {
     fi
 }
 
-pkg_info_func() {
-    
-    sudo dpkg --get-selections > ${scripts_dir}/support/my_installed_apps.list
-    tot_pkgs=$(cat ${scripts_dir}/support/my_installed_apps.list | grep "install" -c)
-    
-    if [ -f /etc/os-release ]; then
-        . /etc/os-release
-        OS=$NAME
-        VER=$VERSION_ID
-    else
-        OS=$(uname -s)
-        VER=$(uname -r)
-    fi
-    
-    KERN=$(uname -r)
-    
-    sed -i 's/^OS=.*/OS=$OS/' ${scripts_dir}/.envrc
-    sed -i 's/^VER=.*/VER=$VER/' ${scripts_dir}/.envrc
-    sed -i 's/^KERN=.*/KERN=$KERN/' ${scripts_dir}/.envrc
-    
-}
-function coloredEcho(){
-    local exp=$1;
-    local color=$2;
-    if ! [[ $color =~ '^[0-9]$' ]] ; then
-        case $(echo $color | tr '[:upper:]' '[:lower:]') in
-            black) color=0 ;;
-            red) color=1 ;;
-            green) color=2 ;;
-            yellow) color=3 ;;
-            blue) color=4 ;;
-            magenta) color=5 ;;
-            cyan) color=6 ;;
-            white|*) color=7 ;; # white or invalid color
-        esac
-    fi
-    tput setaf $color;
-    echo $exp;
-    tput sgr0;
-}
-
 main() {
     get_vars_func
-    coloredEcho
     pkg_info_func
 }
 
 main
-cd /opt/easy-linux
-direnv allow
-sudo direnv allow
 
-cd /opt/easy-linux/support
-direnv allow
-sudo direnv allow
+if [ $(command -v direnv) ]; then
+   cd /opt/easy-linux
+   direnv allow
+   sudo direnv allow
+
+   cd /opt/easy-linux/support
+   direnv allow
+   sudo direnv allow
