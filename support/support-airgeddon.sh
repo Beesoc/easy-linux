90612056703c301e0d7137cb6c2408d58aa983b8
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-18 15:23:15 -0500

    Update support-airgeddon.sh

--- support/support-airgeddon.sh
+++ support/support-airgeddon.sh
@@ -2,11 +2,11 @@
 # Version: 0.0.2
 # shellcheck source=.envrc
 set -e
-scripts_dir=/opt/easy-linux
-source /opt/easy-linux/.envrc
+scripts_dir="/opt/easy-linux"
+source "/opt/easy-linux/.envrc"
 
 run_airg_func() {
-	if [[ $(which airgeddon >/dev/null 2>&1) ]] && [[ $airg_deps_inst = 1 ]]; then
+	if command -v airgeddon >/dev/null 2>&1 && [[ $airg_deps_inst = 1 ]]; then
 		airg_installed=1
 		export airg_installed=1
 
@@ -20,7 +20,7 @@ run_airg_func() {
 
 	read -n 1 -s -t 300 -p "key to continue "
 	printf "${CY}to the ${WT}Main Menu."
-	source /opt/easy-linux/menu-master.sh
+	source "/opt/easy-linux/menu-master.sh"
 	exit 0
 }
 
@@ -28,17 +28,17 @@ airg_install_func() {
 	if [[ $airg_deps_inst = 0 ]]; then
 		deps_airg_install
 	elif [[ $airg_deps_inst = 1 ]]; then
-		printf "  ${CY}Dependencies all installed.  Continuing.\\n"
+		printf "  ${CY}Dependencies all installed. Continuing.\\n"
 	fi
 	if [[ -d $HOME/Downloads/airgeddon ]]; then
 		printf "   #{WT}Airgeddon ${GN}folder found."
 	elif [[ ! -d $HOME/Downloads/airgeddon ]]; then
-		mkdir $HOME/Downloads/airgeddon
-		git clone --depth 1 https://github.com/v1s1t0r1sh3r3/airgeddon.git $HOME/Downloads/airgeddon
+		mkdir "$HOME/Downloads/airgeddon"
+		git clone --depth 1 https://github.com/v1s1t0r1sh3r3/airgeddon.git "$HOME/Downloads/airgeddon"
 	fi
 
 	# Airgeddon must be run with BASH shell, not SH
-	cd $HOME/Downloads/airgeddon
+	cd "$HOME/Downloads/airgeddon"
 	sudo /bin/bash airgeddon.sh
 	airg_installed=1
 	sudo sed -i "s/airg_installed=.*/airg_installed=$airg_installed/g" "${scripts_dir}/.envrc"
@@ -49,11 +49,11 @@ deps_airg_option() {
 	optpackages=("wpaclean" "crunch" "aireplay-ng" "mdk4" "mdk3" "hashcat" "hostapd" "dhcpd" "isc-dhcp-server" "dhcp-server" "dhcp" "nft" "nftables" "iptables" "ettercap" "ettercap-text-only" "ettercap-graphical" "hashcat-utils" "etterlog" "lighttpd" "dnsmasq" "wash" "reaver" "bully" "pixiewps" "bettercap" "beef" "beef-xss / beef-project" "packetforge-ng" "hostapd-wpe" "asleap / asleep" "john" "openssl" "hcxpcapngtool / hcxtools" "hcxdumptool" "tshark / wireshaek-cli" "xml2")
 
 	for optpackage in "${optpackages[@]}"; do
-		if dpkg -s "$optpackages" >/dev/null 2>&1; then
-			echo "$optpackages is already installed"
+		if dpkg -s "$optpackage" >/dev/null 2>&1; then
+			echo "$optpackage is already installed"
 		else
-			echo "Installing $optpackages"
-			sudo apt-get install --ignore-missing -y "$optpackages"
+			echo "Installing $optpackage"
+			sudo apt-get install --ignore-missing -y "$optpackage"
 		fi
 		airg_deps_inst=1
 		sudo sed -i "s/airg_deps_inst=.*/airg_deps_inst=$airg_deps_inst/g" "${scripts_dir}/.envrc"
@@ -82,7 +82,7 @@ deps_airg_install() {
 deps_airg_check() {
 	printf "\\n${GN} Checking dependencies...\\n"
 	if [[ "${airg_deps_inst}" = 1 ]]; then
-		printf "  ${WT}$Airgeddon dependencies ${CY}are installed.  Continuing...\\n"
+		printf "  ${WT}Airgeddon dependencies ${CY}are installed.  Continuing...\\n"
 		airg_install_func
 	elif [[ "${airg_deps_inst}" = 0 ]]; then
 		printf "  ${WT}Airgeddon dependencies ${CY}not installed.  Installing...\\n"
@@ -99,17 +99,16 @@ deps_airg_check() {
 main() {
 	# Check for aircrack-ng installation
 	clear
-	source ${scripts_dir}/support/support-Banner_func.sh
+	source "${scripts_dir}/support/support-Banner_func.sh"
 
 	# Check for airgeddon installation
-
 	if [[ -f /opt/easy-linux/.envrc ]]; then
 		airg_installed=$(grep "airg_installed" /opt/easy-linux/.envrc | cut -d "=" -f 2)
 		if [[ $airg_installed -eq 1 ]]; then
 			printf "Airgeddon is already installed. Skipping installation.\\n"
 			run_airg_func
 		fi
-		if [[ $airg__deps_inst -eq 0 ]]; then
+		if [[ $airg_deps_inst -eq 0 ]]; then
 			printf "Airgeddon dependencies not installed. Installing...\\n"
 			if [[ $airg_deps_inst = 1 ]]; then
 				airg_install_func
@@ -119,7 +118,7 @@ main() {
 					printf "Aircrack-NG is already installed. Skipping installation.\\n"
 					deps_airg_install
 				elif [[ $airc_deps_inst = 0 ]]; then
-					source ${scripts_dir}/support/support-aircrack2.sh
+					source "${scripts_dir}/support/support-aircrack2.sh"
 					deps_airg_install
 				fi
 			fi
