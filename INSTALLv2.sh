145ae6df715ccb561c3aa7943d894c1a84d8692c
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-21 02:38:24 -0500

    Add files via upload

--- INSTALLv2.sh
+++ INSTALLv2.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # New app installer from Github
 function help {
-  echo 
-  echo "Usage: $0 [options]"
-  echo "  This is the help menu for the app installer for Beesoc's Easy Linux Loader."
-  echo "  No extra parameters can be applied to the installer at this point."
-  echo "  Make it executable with 'sudo chmod +x INSTALLv2.sh' then run with .INSTALLv2.sh"
-  echo
-  echo
-echo "Options:"
-  echo "  -h, --help: Display this help information."
-  echo "  -v, --version: Display the version information."
-  echo
+	echo
+	echo "Usage: $0 [options]"
+	echo "  This is the help menu for the app installer for Beesoc's Easy Linux Loader."
+	echo "  No extra parameters can be applied to the installer at this point."
+	echo "  Make it executable with 'sudo chmod +x INSTALLv2.sh' then run with .INSTALLv2.sh"
+	echo
+	echo
+	echo "Options:"
+	echo "  -h, --help: Display this help information."
+	echo "  -v, --version: Display the version information."
+	echo
 }
 
 if [[ $1 == "-h" || $1 == "--help" ]]; then
-  help
-  exit 0
+	help
+	exit 0
 elif [[ $1 == "-v" || $1 == "--version" ]]; then
-  echo "The script is version 0.0.2."
-  exit 0
+	echo "The script is version 0.0.2."
+	exit 0
 fi
 # Version: 0.0.2
 
@@ -37,153 +37,158 @@ OGH='\e[0;30;44m'
 NC='\e[0m'
 
 Banner_func() {
-  printf "${WT}\\n"
-printf "${OGH}▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀${OGG}${RED}\\n"
-printf "    ▄████▄${BK}╗    ${RED}▄████▄${BK}╗   ${RED}▄████▄${BK}╗    ${RED}▄████▄${BK}╗     ${RED}▄███▄${BK}╗     ${RED}▄███▄${BK}╗  ${RED}██${BK}╗  ${RED}▄████▄${BK}╗    ${NC}\\n"
-printf "${OGG}    ${OG}██${BK}═══${OG}██${BK}╝   ${OG}██${BK}╔═══╝   ${OG}██${BK}╔═══╝   ${OG}██${BK}╔════╝    ${OG}██${BK}╔══${OG}██${BK}╗   ${OG}██${BK}╔══${OG}▀${BK}╝   ${OG}▀${BK}╝ ${OG}██${BK}╔════╝${NC}${OGG}    ${NC}\\n"
-printf "${OGG}    ${OG}██████${BK}╝    ${OG}█████${BK}╗    ${OG}█████${BK}╗     ${OG}▀████▄${BK}╗    ${OG}██${BK}║  ${OG}██${BK}║   ${OG}██${BK}║           ${OG}▀████▄${BK}╗${OGG}    ${NC}\\n${NC}"
-printf "${OGG}    ${OG}██${BK}═══${OG}██    ${OG}██${BK}╔══╝${OG}    ██${BK}╔══╝      ╚═══${OG}██${BK}║   ${OG}██${BK}║  ${OG}██${BK}╝   ${OG}██${BK}║  ${OG}▄${BK}╗        ╚═══${OG}██${BK}║${OGF}   \\n"
-printf "    ${OGF}▀████▀${BK}╝    ${OGF}▀████▀${BK}╗   ${OGF}▀████▀${BK}╗    ${OGF}▀████▀${BK}╝     ${OGF}▀███▀${BK}╝${OGF}     ▀███▀${BK}╝${OGF}       ▀████▀${BK}╝   ${BK} \\n"
-printf "     ${BK}╚══╝       ╚═══╝     ╚═══╝      ╚══╝        ╚═╝        ╚═╝          ╚══╝      \\n${OGG}"
-printf "${OGG}    ${OGG}▄████▄${BK}╗   ${OGG}▄█▄${BK}╗    ${OGG}▄███▄${BK}╗  ${OGG}██${BK}╗   ${OGG}██${BK}╗   ${OGG}▄█${BK}╗    ${OGG}▄█${BK}╗ ${OGG}▄█${BK}╗  ${OGG}▄█${BK}╗ ${OGG}▄█${BK}╗ ${OGG} ▄█${BK}╗ ${OGG}██▄${BK}╗  ${OGG}▄██${BK}╗  \\n"
-printf "${GN}    ${GN}██${BK}╔═══╝  ${GN}██${BK}║${GN}██${BK}╗  ${GN}██${BK}╔═══╝  ${GN}▀██▄▄██▀${BK}╝   ${GN}██${BK}║    ${GN}██${BK}║ ${GN}█${BK}${GN}██▄${BK}╗${GN}██${BK}║${GN} ██${BK}║  ${GN}██${BK}║  ${GN}▀██▄██▀${BK}╝${GN}   \\n"
-printf "${OGG}    ${GN}█████${BK}╗  ${GN}███▀███${BK}║  ${GN}▀███▄${BK}╗    ${GN}▀██▀${BK}╔╝    ${GN}██${BK}║    ${GN}██${BK}║ ${GN}██▀████${BK}║ ${GN}██${BK}║  ${GN}██${BK}║   ${GN} ███${BK}║     \\n  "  
-printf "  ${GN}██${BK}╔══╝  ${GN}██${BK}╔══${GN}${OGG}${GN}██${BK}║      ${GN}██${BK}║    ${GN}██${BK}║      ${GN}██${BK}║    ${GN}██${BK}║ ${GN}██${BK}║ ${GN}███${BK}║ ${GN}██${BK}║  ${GN}██${BK}║  ${GN}▄██${BK}${GN}▀██${BK}${BK}╗    \\n"
-printf "  ${WT}${OGG}   ${WT}▀███▀${BK}╝ ${WT}██${BK}║  ${WT}██${BK}║  ${WT}▀███▀${BK}╝     ${WT}██${BK}║      ${WT}▀████${BK}╗ ${WT}██${BK}║ ${WT}██${BK}║  ${WT}██${BK}║  ${WT}▀███▀${BK}╝  ${WT}██▀${BK}╝  ${WT}▀██${BK}╗  \\n   ${BK}"
-printf "${OGG}  ${BK} ╚══╝  ${BK}╚═╝  ${BK}╚═╝   ${BK}╚═╝       ${BK}╚═╝       ${BK}╚═══╝ ${BK}╚═╝ ${BK}╚═╝  ${BK}╚═╝   ${BK}╚══╝   ${BK}╚═╝    ${BK}╚═╝  ${OGH}
+	printf "${WT}\\n"
+	printf "${OGH}▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀${OGG}${RED}\\n"
+	printf "    ▄████▄${BK}╗    ${RED}▄████▄${BK}╗   ${RED}▄████▄${BK}╗    ${RED}▄████▄${BK}╗     ${RED}▄███▄${BK}╗     ${RED}▄███▄${BK}╗  ${RED}██${BK}╗  ${RED}▄████▄${BK}╗    ${NC}\\n"
+	printf "${OGG}    ${OG}██${BK}═══${OG}██${BK}╝   ${OG}██${BK}╔═══╝   ${OG}██${BK}╔═══╝   ${OG}██${BK}╔════╝    ${OG}██${BK}╔══${OG}██${BK}╗   ${OG}██${BK}╔══${OG}▀${BK}╝   ${OG}▀${BK}╝ ${OG}██${BK}╔════╝${NC}${OGG}    ${NC}\\n"
+	printf "${OGG}    ${OG}██████${BK}╝    ${OG}█████${BK}╗    ${OG}█████${BK}╗     ${OG}▀████▄${BK}╗    ${OG}██${BK}║  ${OG}██${BK}║   ${OG}██${BK}║           ${OG}▀████▄${BK}╗${OGG}    ${NC}\\n${NC}"
+	printf "${OGG}    ${OG}██${BK}═══${OG}██    ${OG}██${BK}╔══╝${OG}    ██${BK}╔══╝      ╚═══${OG}██${BK}║   ${OG}██${BK}║  ${OG}██${BK}╝   ${OG}██${BK}║  ${OG}▄${BK}╗        ╚═══${OG}██${BK}║${OGF}   \\n"
+	printf "    ${OGF}▀████▀${BK}╝    ${OGF}▀████▀${BK}╗   ${OGF}▀████▀${BK}╗    ${OGF}▀████▀${BK}╝     ${OGF}▀███▀${BK}╝${OGF}     ▀███▀${BK}╝${OGF}       ▀████▀${BK}╝   ${BK} \\n"
+	printf "     ${BK}╚══╝       ╚═══╝     ╚═══╝      ╚══╝        ╚═╝        ╚═╝          ╚══╝      \\n${OGG}"
+	printf "${OGG}    ${OGG}▄████▄${BK}╗   ${OGG}▄█▄${BK}╗    ${OGG}▄███▄${BK}╗  ${OGG}██${BK}╗   ${OGG}██${BK}╗   ${OGG}▄█${BK}╗    ${OGG}▄█${BK}╗ ${OGG}▄█${BK}╗  ${OGG}▄█${BK}╗ ${OGG}▄█${BK}╗ ${OGG} ▄█${BK}╗ ${OGG}██▄${BK}╗  ${OGG}▄██${BK}╗  \\n"
+	printf "${GN}    ${GN}██${BK}╔═══╝  ${GN}██${BK}║${GN}██${BK}╗  ${GN}██${BK}╔═══╝  ${GN}▀██▄▄██▀${BK}╝   ${GN}██${BK}║    ${GN}██${BK}║ ${GN}█${BK}${GN}██▄${BK}╗${GN}██${BK}║${GN} ██${BK}║  ${GN}██${BK}║  ${GN}▀██▄██▀${BK}╝${GN}   \\n"
+	printf "${OGG}    ${GN}█████${BK}╗  ${GN}███▀███${BK}║  ${GN}▀███▄${BK}╗    ${GN}▀██▀${BK}╔╝    ${GN}██${BK}║    ${GN}██${BK}║ ${GN}██▀████${BK}║ ${GN}██${BK}║  ${GN}██${BK}║   ${GN} ███${BK}║     \\n  "
+	printf "  ${GN}██${BK}╔══╝  ${GN}██${BK}╔══${GN}${OGG}${GN}██${BK}║      ${GN}██${BK}║    ${GN}██${BK}║      ${GN}██${BK}║    ${GN}██${BK}║ ${GN}██${BK}║ ${GN}███${BK}║ ${GN}██${BK}║  ${GN}██${BK}║  ${GN}▄██${BK}${GN}▀██${BK}${BK}╗    \\n"
+	printf "  ${WT}${OGG}   ${WT}▀███▀${BK}╝ ${WT}██${BK}║  ${WT}██${BK}║  ${WT}▀███▀${BK}╝     ${WT}██${BK}║      ${WT}▀████${BK}╗ ${WT}██${BK}║ ${WT}██${BK}║  ${WT}██${BK}║  ${WT}▀███▀${BK}╝  ${WT}██▀${BK}╝  ${WT}▀██${BK}╗  \\n   ${BK}"
+	printf "${OGG}  ${BK} ╚══╝  ${BK}╚═╝  ${BK}╚═╝   ${BK}╚═╝       ${BK}╚═╝       ${BK}╚═══╝ ${BK}╚═╝ ${BK}╚═╝  ${BK}╚═╝   ${BK}╚══╝   ${BK}╚═╝    ${BK}╚═╝  ${OGH}
 ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄\\n"
-printf "${NC}${CY}"
+	printf "${NC}${CY}"
 
-#  █ ▌▀ ▄ ╚ ╝ ╔ ╗ ═ ║  Characters used in the banner.
-  #
-}  
-
+	#  █ ▌▀ ▄ ╚ ╝ ╔ ╗ ═ ║  Characters used in the banner.
+	#
+}
 
 cleanup_func() {
-# Step 7 function. End.
-printf "${WT} [*] ${CY}Please Wait while I cleanup some files used in the installation -${NC} \\n"
-printf "${WT}..."; sleep 1; printf "...Almost done\\n" 
+	# Step 7 function. End.
+	printf "${WT} [*] ${CY}Please Wait while I cleanup some files used in the installation -${NC} \\n"
+	printf "${WT}..."
+	sleep 1
+	printf "...Almost done\\n"
 
-  clear
-  Banner_func
-  printf "   ${CY}[*] ${WT}Beesoc's Easy Linux Loader ${GN}has been installed.\\n\\n" 
-  printf "   ${CY}Use the option on your ${WT}Apps menu ${CY}or enter [ ${WT}menu-master.sh${CY} ]\\n"
-  printf "   from ${WT}any Terminal ${CY}to access. Thanks for using ${WT}Beesoc's Easy Linux Loader${CY}!\\n${CY}" 
+	clear
+	Banner_func
+	printf "   ${CY}[*] ${WT}Beesoc's Easy Linux Loader ${GN}has been installed.\\n\\n"
+	printf "   ${CY}Use the option on your ${WT}Apps menu ${CY}or enter [ ${WT}menu-master.sh${CY} ]\\n"
+	printf "   from ${WT}any Terminal ${CY}to access. Thanks for using ${WT}Beesoc's Easy Linux Loader${CY}!\\n${CY}"
 
-rc=$(cat ~/.bashrc | grep "truecolor" -c)
-zc=$(cat ~/.zshrc | grep "truecolor" -c)
-if [[ $rc > 0 ]]; then   
-        sudo echo "export COLORTERM=truecolor" >> ~/.bashrc
-        sudo echo "export COLORFGBG=15;0" >> ~/.bashrc
-fi
-if [[ $zc > 0 ]]; then   
-        sudo echo "export COLORTERM=truecolor" >> ~/.zshrc
-        sudo echo "export COLORFGBG=15;0" >> ~/.zshrc
-fi
+	rc=$(cat ~/.bashrc | grep "truecolor" -c)
+	zc=$(cat ~/.zshrc | grep "truecolor" -c)
+	if [[ $rc = 0 ]]; then
+		sudo echo "export COLORTERM=truecolor" >>~/.bashrc
+		sudo echo "export COLORFGBG=15;0" >>~/.bashrc
+	fi
+	if [[ $zc = 0 ]]; then
+		sudo echo "export COLORTERM=truecolor" >>~/.zshrc
+		sudo echo "export COLORFGBG=15;0" >>~/.zshrc
+	fi
 
-printf "\\n${CY}      Hey ${WT}$USER${CY}, would you like to launch \\n${WT}       "
-read -n 1 -p "Beesoc's Easy Linux Loader now? [Y/n] " launchnow
-launchnow=${launchnow:-Y}
-if [[ $launchnow =~ ^[Yy]$ ]]; then
-  printf "${GN}\\n   Starting Beesoc's Easy Linux now....\\n"
-        source /opt/easy-linux/menu-master.sh
-else
-  printf "    ${RED}Exiting.\\n"
-  exit 0
-fi
+	printf "\\n${CY}      Hey ${WT}$USER${CY}, would you like to launch \\n${WT}       "
+	read -n 1 -p "Beesoc's Easy Linux Loader now? [Y/n] " launchnow
+	launchnow=${launchnow:-Y}
+	if [[ $launchnow =~ ^[Yy]$ ]]; then
+		printf "${GN}\\n   Starting Beesoc's Easy Linux now....\\n"
+		source /opt/easy-linux/menu-master.sh
+	else
+		printf "    ${RED}Exiting.\\n"
+		exit 0
+	fi
 }
 
 git_files_func() {
-# step 6 function.
-  echo 
-  printf "  ${WT} \\n  [*] ${GN}Cloning remote Git repo.${OG}\\n  "; sleep 1
-  sudo git clone https://github.com/Beesoc/easy-linux.git /opt/easy-linux
+	# step 6 function.
+	printf "  ${WT} \\n  [*] ${GN}Cloning remote Git repo.${OG}\\n  "
+	sleep 1
+	sudo git clone https://github.com/Beesoc/easy-linux.git /opt/easy-linux
 
-sudo chown -vR $USER:0 /opt/easy-linux
-sudo chmod +x /opt/easy-linux/support/*.sh
-sudo mv /opt/easy-linux/install/easy-linux.desktop /usr/share/applications/
-sudo mv -t /opt/easy-linux /opt/easy-linux/install/*
-sudo chmod +x /opt/easy-linux/*.sh
-sudo cp -f /opt/easy-linux/menu-master.sh /usr/bin/
+	sudo chown -vR $USER:0 /opt/easy-linux
+	sudo chmod +x /opt/easy-linux/support/*.sh
+	sudo mv /opt/easy-linux/install/easy-linux.desktop /usr/share/applications/
+	sudo chmod +x /opt/easy-linux/install/*.sh
+	sudo cp -f /opt/easy-linux/install/* /opt/easy-linux
+	sudo cp -f /opt/easy-linux/menu-master.sh /usr/bin/
 
-cleanup_func
+	cleanup_func
 }
 
 install_func() {
-# Step 5 function.
-# Banner_func
-printf "${WT}\\n  [*] ${GN}Dependencies satisfied.\\n\\n  ${WT}[*]${WT} "; sleep 1
-read -n 1 -p "Do you want to install Easy Linux Loader? [Y/n] " choiceez
-          choiceez=${choiceez:-Y}
-            if [[ "$choiceez" =~ ^[Yy]$ ]]; then
-          #if [[ $choiceez = "Y" ]] || [[ $choiceez = "y" ]]; then
-              printf "\\n${WT}  [*] ${CY}Installation confirmed..."; sleep 1; printf "..Please wait..\\n";  sleep 1
-            elif [[ "$choiceez" =~ ^[Nn]$ ]]; then
-            #elif [[ $choiceez = "n" ]] || [[ $choiceez = "N" ]]; then
-              printf "\\n${RED}  [*] ${OG}Installation rejected...\\n"; sleep 1; printf "${CY}..Please wait...\\n"
-              exit 0
-            else
-              printf "\\n${WT}  [*]  ${RED}Invalid Selection. Exiting.\\n"
-              exit 0
-            fi
-     git_files_func
+	# Step 5 function.
+	# Banner_func
+	printf "${WT}\\n  [*] ${GN}Dependencies satisfied.\\n\\n  ${WT}[*]${WT} "
+	sleep 1
+	read -n 1 -p "Do you want to install Easy Linux Loader? [Y/n] " choiceez
+	choiceez=${choiceez:-Y}
+	if [[ "$choiceez" =~ ^[Yy]$ ]]; then
+		#if [[ $choiceez = "Y" ]] || [[ $choiceez = "y" ]]; then
+		printf "\\n${WT}  [*] ${CY}Installation confirmed...\\n"
+		sleep 1
+		printf "..Please wait..\\n"
+		sleep 1
+	elif [[ "$choiceez" =~ ^[Nn]$ ]]; then
+		#elif [[ $choiceez = "n" ]] || [[ $choiceez = "N" ]]; then
+		printf "\\n${RED}  [*] ${OG}Installation rejected...\\n"
+		sleep 1
+		printf "${CY}..Please wait...\\n"
+		exit 0
+	else
+		printf "\\n${WT}  [*]  ${RED}Invalid Selection. Exiting.\\n"
+		exit 0
+	fi
+	git_files_func
 }
 check_directories_func() {
-# Step 2 or 4 function.
-if [[ -d /opt/easy-linux ]]; then
-    printf "  ${WT}[*] ${GN}/opt/easy-linux ${CY}directory found.\\n"
-    printf "  ${WT}[*] ${GN}Removing and recloning repository.\\n" 
-    sleep 1
-    sudo rm -fr /opt/easy-linux
-elif [[ ! -d /opt/easy-linux ]]; then
-#    sudo chown -v 1000:1000 /opt
-    printf " ${WT} [*] ${WT}/opt/easy-linux ${CY}directory not found. Cloning repo into that folder.\\n"; sleep 1
-fi  
-install_func
+	# Step 2 or 4 function.
+	if [[ -d /opt/easy-linux ]]; then
+		printf "  ${WT}[*] ${GN}/opt/easy-linux ${CY}directory found.\\n"
+		printf "  ${WT}[*] ${GN}Removing and recloning repository.\\n"
+		sleep 1
+		sudo rm -fr /opt/easy-linux
+	elif [[ ! -d /opt/easy-linux ]]; then
+		printf " ${WT} [*] ${WT}/opt/easy-linux ${CY}directory not found. Cloning repo into that folder.\\n"
+		sleep 1
+	fi
+	install_func
 }
 
 direnv_func() {
-# Step 3 or skip function.
-               read -n 1 -p "DIRENV is not installed. Do you want me to install it? [Y/n] " choicedirenv
-        choicedirenv=${choicedirenv:-Y}
-                if [[ "$choicedirenv" =~ ^[Yy]$ ]]; then
-                    printf "${GN}\\n  Continuing...\\n" 
-                    sleep 1
-                    printf "  ${OG}This step may take a few minutes...\\n"
-                    sleep 1
-                    printf "  ${OG}Please wait.\\n"
-                    sudo apt update
-                    sudo apt install -y direnv
-                else
-                   printf "${GN} Not Needed.  Continuing.\\n"
-                
-                fi
-       check_directories_func
+	# Step 3 or skip function.
+	read -n 1 -p "DIRENV is not installed. Do you want me to install it? [Y/n] " choicedirenv
+	choicedirenv=${choicedirenv:-Y}
+	if [[ "$choicedirenv" =~ ^[Yy]$ ]]; then
+		printf "${GN}  Continuing...\\n"
+		sleep 1
+		printf "  ${OG}This step may take a few minutes...\\n"
+		sleep 1
+		printf "  ${OG}Please wait.\\n"
+		sudo apt update
+		sudo apt install -y direnv
+	else
+		printf "${GN} Not Needed.  Continuing.\\n"
+
+	fi
+	check_directories_func
 }
 
 main() {
-# 1.  script starts executing here.
-clear
-Banner_func
+	# 1.  script starts executing here.
+	clear
+	Banner_func
 
-printf "\\n${GN}    Welcome to the Installer for ${WT}Beesoc's Easy Linux${GN}    Press ${RED}[ctrl+c] ${GN}to cancel\\n${CY}\\n    " 
-
-printf "${CY}  Do you want to ${WT}check dependencies ${CY}"
-read -n 1 -p "for Beesoc's Easy Linux Loader? [Y/n] " install
-install=${install:-Y}
-if [[ "$install" =~ ^[Yy]$ ]]; then
-  printf "\\n  ${WT}[*] ${CY}Loading...Please Wait...\\n"
-else
-  printf "  ${RED}   Exiting.\\n"
-  exit 0
-fi
-# check for requirements.
-    if $(command -v /usr/bin/direnv >/dev/null 2>&1); then
-       check_directories_func
-    else
-    direnv_func
-    fi
+	printf "\\n${GN}    Welcome to the Installer for ${WT}Beesoc's Easy Linux${GN}    Press ${RED}[ctrl+c] ${GN}to cancel\\n${CY}\\n"
+	printf "${CY}  Do you want to ${WT}check dependencies ${CY}"
+	read -n 1 -p "for Beesoc's Easy Linux Loader? [Y/n] " install
+	install=${install:-Y}
+	if [[ "$install" =~ ^[Yy]$ ]]; then
+		printf "\\n  ${WT}[*] ${CY}Loading, Please Wait\\n"
+	else
+		printf "  ${RED}   Exiting.\\n"
+		exit 0
+	fi
+	# check for requirements.
+	if $(command -v /usr/bin/direnv >/dev/null 2>&1); then
+		check_directories_func
+	else
+		direnv_func
+	fi
 }
 
 main
-
---
+++ update_scripts.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+# update-scripts.sh
+scripts_dir=/opt/easy-linux
+set -e
+# Version: 0.0.2
+
+# Read the required versions from version-easy-linux.sh
+source "${scripts_dir}/version-easy-linux.sh"
+source "${scripts_dir}/.envrc"
+
+sudo chown -vR "$USER:0" "${scripts_dir}"
+
+if [[ ! -d "${scripts_dir}/tmp" ]]; then
+    mkdir "${scripts_dir}/tmp"
+    if [[ ! -d "${scripts_dir}/tmp/support" ]]; then    
+        mkdir "${scripts_dir}/tmp/support"
+    fi
+    if [[ ! -d "${scripts_dir}/tmp/install" ]]; then
+        mkdir "${scripts_dir}/tmp/install"
+    fi 
+fi
+
+# Array to store the script names
+script_files=(
+    "install/menu-master.sh"
+    "install/menu-hacking.sh"
+    "install/menu-customize.sh"
+    "install/menu-apps.sh"
+    "install/menu-pwn.sh"
+    "install/menu-upload-hashes.sh"
+    ".envrc"
+    "README.md"
+    "support/version-support.sh"
+    "install/menu-trouble.sh"
+    "INSTALLv2.sh"
+    ".shellcheckrc"
+    "update_scripts.sh"
+    "support/.whoami.sh"
+    "support/support-aircrack2.sh"
+    "support/support-airgeddon.sh"
+    "support/support-autojump.sh"
+    "support/support-Banner_func.sh"
+    "support/support-banner.sh"
+    "support/support-docker.sh"
+    "support/support-fatrat.sh"
+    "support/support-fix-my-perm.sh"
+    "support/support-getvars.sh"
+    "support/support-hackingtool.sh"
+    "support/support-hcxdump.sh"
+    "support/support-ican-haz-internet.sh"
+    "support/support-inst-standard.sh"
+    "support/support-linux_connection_script.sh"
+    "support/support-makeWordlist.sh"
+    "support/support-monitor.sh"
+    "support/support-monDOWN.sh"
+    "support/support-makeWordlist.sh"
+    "support/support-nano.sh"
+    "support/support-netadapter.sh"
+    "support/support-ohmy.sh"
+    "support/support-Prompt_func.sh"
+    "support/support-sysinfo.sh"
+    "support/support-trap-wifi.sh"
+    "support/support-updates.sh"
+    "support/support-webmin.sh"
+    "support/support-wifite.sh"
+    "support/support-wpaUP.sh"
+    "support/support-wpaDOWN.sh"
+    "support/version-support.sh"
+    "support/trap-master.sh"
+    "support/update_scripts.sh"
+    "support/version-support.sh"
+)
+
+# Associative array to store the version information
+declare -A version_info
+
+# Function to update a script
+update_script() {
+    local script_name=$1
+    local required_version=$2
+
+    # Get the current version of the script
+    local current_version=$(grep -m 1 -o -P "(?<=# Version: ).*" "$script_name")
+
+    # Compare versions
+    if [[ $current_version != "$required_version" ]]; then
+        printf "${OG}  [-] ${PL}Updating $script_name...${NC}\\n"
+        
+        # Download the updated script from GitHub or any other source
+        # Replace the existing script with the updated version
+        # For example:
+        sudo curl -o "${scripts_dir}/tmp/$script_name" -sSLO "https://raw.githubusercontent.com/Beesoc/easy-linux/main/$script_name"  
+        if [[ -e "${scripts_dir}/tmp/$script_name" ]]; then
+            sudo chmod +x "${scripts_dir}/tmp/$script_name"
+            sudo mv "${scripts_dir}/tmp/$script_name" "${scripts_dir}/$script_name"
+        fi
+
+        printf "${GN}  [*] ${CY}Script $script_name updated.\\n"
+
+        # Store the version information in the associative array
+        version_info["$script_name"]="Current Ver: $current_version. New Ver: $required_version"
+    else
+        printf "${GN}  [*] No update needed for $script_name${GN}.\\n"
+    fi
+}
+
+# Loop through the scripts and check for updates
+for script_name in "${script_files[@]}"; do
+    # Check if the script exists
+    if [[ -f $script_name ]]; then
+        # Get the required version from the associative array
+        required_version="${required_versions[$script_name]}"
+
+        if [[ -n $required_version ]]; then
+            update_script "$script_name" "$required_version"
+        else
+            printf "${RED}Required version not found for $script_name in version-easy-linux.sh.${RED}\\n"
+        fi
+    else
+        printf "${RED}Script $script_name not found.${NC}\\n"
+    fi
+done
+
+# Print update report
+# Print update report
+clear
+source "${scripts_dir}/support/support-Banner_func.sh"
+printf "${OG}|                                                                                 |\n"
+printf "${OG}|------------------------------ ${GN}| Update Report | ${OG}--------------------------------|${OG}\n"
+#printf "|                                                                                 |\n"
+printf "    ${WT}User: ${OG}$USER     |     ${WT}Computer: ${OG}$computername     |     ${WT}Date: ${OG}$(date +"%Y-%m-%d")${NC}\n"
+printf "    ${WT}Total updates: ${PL}${#version_info[@]}${NC}\n"
+printf "${OG}|_________________________________________________________________________________|${CY}\n"
+
+# Define the column widths
+script_name_width=40
+version_info_width=40
+
+# Print column headers
+printf "|                                                                                 |${CY}\n    "
+printf "%-${script_name_width}s %-${version_info_width}s\n" "${BOLD}Script Name" "${BOLD}Version Information"
+
+printf "|                                                                                 |${WT}\n"
+# Loop through the version info array and print the details in columns
+for script_name in "${!version_info[@]}"; do
+    printf " %-${script_name_width}s %-${version_info_width}s\n" "$script_name" "${version_info[$script_name]}"
+done
+printf "${GN}|                                                                                 |\n"
+printf "${GN}|_________________________________________________________________________________|\n"
+echo
+printf "  ${GN}Press ${WT}any ${GN}key to continue${NC}\n"
+echo
+read -n 1 -s -r -t 300
+
+# Clean up temporary files
+rm -rf "${scripts_dir}/tmp"
