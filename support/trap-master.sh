8213a7fd1078d897e4cb95ea3845b9da3645371a
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-23 13:20:48 -0500

    Update trap-master.sh

--- support/trap-master.sh
+++ support/trap-master.sh
@@ -3,6 +3,22 @@
 # Version: 0.0.2
 scripts_dir=/opt/easy-linux
 source ${scripts_dir}/.envrc
+
+if sudo iwconfig 2>/dev/null | grep -q "Mode:Monitor"; then
+    # Command to execute when a WiFi adapter is in monitor mode
+    printf "${CY}  At least 1 WiFi adapter is ${WT}still in monitor mode${CY}. \\n  [?] "
+    read -n 1 -p "Should I reenable Managed mode?" managed 
+    managed=${managed:=Y}
+    if [[ "$managed" ~= ^[yY]$ ]]; then
+        source ${scripts_dir}/support/support-monitor.sh
+    elif [[ "$managed" ~= ^[nN]$ ]]; then
+        printf "  ${WT}$USER ${OG}has selected to remain in Monitor mode."
+        printf "  ${CY}You can toggle Monitor mode On/Off in the Easy Linux Hacking Menu."
+        echo
+else
+    clear
+fi
+
 echo
 printf "${GN}\\n*********************************** Exiting ************************************\\n"
 printf "             ${WT}--  ${GN}Thanks for using Beesoc's Easy Linux Loader  ${WT}--\\n\\n"
