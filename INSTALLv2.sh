3a7342eb79d8471884023467b22033807428e821
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-23 12:13:23 -0500

    Update INSTALLv2.sh

--- INSTALLv2.sh
+++ INSTALLv2.sh
@@ -215,6 +215,7 @@ git_files_func() {
 
         sudo chown -vR $USER:0 /opt/easy-linux
         sudo chmod +x /opt/easy-linux/support/*.sh
+        sudo chmod +x /opt/easy-linux/*.sh
         sudo mv /opt/easy-linux/install/easy-linux.desktop /usr/share/applications/
         sudo chmod +x /opt/easy-linux/install/*.sh
         sudo cp -f /opt/easy-linux/install/menu-master.sh /usr/bin/
