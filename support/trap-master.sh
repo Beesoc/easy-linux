61693b3092832f1327c1489a909867fccf1346b8
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-23 12:24:18 -0500

    Update trap-master.sh

--- support/trap-master.sh
+++ support/trap-master.sh
@@ -6,7 +6,7 @@ source ${scripts_dir}/.envrc
 echo
 printf "${GN}\\n*********************************** Exiting ************************************\\n"
 printf "             ${WT}--  ${GN}Thanks for using Beesoc's Easy Linux Loader  ${WT}--\\n\\n"
-printf "           ${CY}Use the icon on your ${WT}Apps menu ${CY}or enter [ ${WT}menu-master.sh${CY} ]\\n"
+printf "           ${CY}Use the icon on your ${WT}Apps menu ${CY}or enter [ ${WT}easy-linux.sh${CY} ]\\n"
 printf "                  from ${WT}any Terminal ${CY}to access Easy Linux again. \\n${CY}"
 printf "${GN}********************************************************************************${NC}\\n"
 echo
