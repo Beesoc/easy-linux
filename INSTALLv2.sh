a3bd7321787cfa100088846dc7116457aff79deb
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-23 12:03:59 -0500

    Update INSTALLv2.sh

--- INSTALLv2.sh
+++ INSTALLv2.sh
@@ -288,7 +288,7 @@ main() {
         command_not_found_handle
 
         printf "\\n${GN}    Welcome to the Installer for ${WT}Beesoc's Easy Linux${GN}    Press ${RED}[ctrl+c] ${GN}to cancel\\n${CY}\\n"
-        printf "${CY}  Do you want to ${WT}check dependencies ${CY}"
+        printf "${CY}  [?] Do you want to ${WT}check dependencies ${CY}"
         read -n 1 -p "for Beesoc's Easy Linux Loader? [Y/n] " install
         install=${install:-Y}
         if [[ "$install" =~ ^[Yy]$ ]]; then
