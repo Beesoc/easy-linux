634ecdad63bab601b7a80e1bb622068e999ef53f
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-23 11:31:22 -0500

    Update trap-master.sh

--- support/trap-master.sh
+++ support/trap-master.sh
@@ -8,5 +8,5 @@ printf "${GN}\\n*********************************** Exiting ********************
 printf "             ${WT}--  ${GN}Thanks for using Beesoc's Easy Linux Loader  ${WT}--\\n\\n"
 printf "           ${CY}Use the icon on your ${WT}Apps menu ${CY}or enter [ ${WT}menu-master.sh${CY} ]\\n"
 printf "                  from ${WT}any Terminal ${CY}to access Easy Linux again. \\n${CY}"
-printf "${GN}********************************************************************************\\n"
+printf "${GN}********************************************************************************${NC}\\n"
 echo
