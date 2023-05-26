844f9bf966c07dad9d83ece18cf7037c0ff83f5c
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-26 00:28:27 -0500

    Add files via upload

--- support/support-trap-wifi.sh
+++ support/support-trap-wifi.sh
@@ -8,20 +8,31 @@ source "${scripts_dir}/.envrc"
 # get a list of wireless network interfaces
 interfaces=$(iwconfig 2>/dev/null | grep -o "^[^ ]*")
 
+if [ $(iw dev | grep "monitor" -c) -ne 0 ]; then
+    printf "  ${CY}You have at least 1 wifi adapter in Monitor mode.${OG}\\n"
+    read -n 1 -p "  Do you want to change back to Manage mode? [Y/n] ---->" cngmode
+    cngmode=${cngmode:-y}
+    printf "\\n${CY}"
+    if [ "$cngmode" = "y" ] || [ "$cngmode" = "Y" ]; then
 # iterate over the interfaces and check their mode
-for iface in $interfaces; do
-        mode=$(iwconfig $iface 2>/dev/null | grep -o "Mode:[^ ]*" | cut -f2 -d:)
+    for iface in $interfaces; do
+    #	mode=$(iwconfig $iface 2>/dev/null | grep -o "Mode:[^ ]*" | cut -f2 -d:)
+        mode=$(iw dev | grep "monitor" | cut -f2 -d" ")
 
-        if [ "$mode" = "Monitor" ]; then
-                # switch the interface to managed mode
-                sudo iwconfig $iface mode managed
+          sudo systemctl stop NetworkManager && sudo systemctl stop wpa_supplicant.service
+          sudo iw $iface set type managed
 
-                # restart wpa_supplicant and Network Manager services
-                sudo systemctl restart wpa_supplicant.service NetworkManager.service
-        printf "\\n"
-                printf "  ${GN}[*] ${CY}Interface ${WT}$iface ${CY}switched to managed mode.\\n"
-                printf "  ${GN}[*] ${CY}NetworkManager.service and wpa_supplicant.service restarted.\\n"
-        fi
+	    	# restart wpa_supplicant and Network Manager services
+    		sudo systemctl start wpa_supplicant.service && sudo systemctl start NetworkManager.service
+            printf "\\n"
 
-done
+    done
+       	printf "  ${GN}[*] ${CY}Interfaces ${CY}switched to managed mode.\\n"
+	   	printf "  ${GN}[*] ${CY}NetworkManager.service and wpa_supplicant.service restarted.\\n"
+    else
+       printf " ${WT} $USER ${OG}has selected to stay in Monitor mode.\\n"
+       printf " \\n"
+  	fi
+
+fi
 trap "source ${scripts_dir}/support/trap-master.sh" EXIT
--- support/trap-master.sh
+++ support/trap-master.sh
@@ -6,21 +6,10 @@ source ${scripts_dir}/.envrc
 
 if sudo iwconfig 2>/dev/null | grep -q "Mode:Monitor"; then
     # Command to execute when a WiFi adapter is in monitor mode
-    printf "${CY}  At least 1 WiFi adapter is ${WT}still in monitor mode${CY}. \\n  [?] "
-    read -n 1 -p "Should I reenable Managed mode?" managed 
-    managed=${managed:=Y}
-    if [[ "$managed" =~ ^[yY]$ ]]; then
-        source ${scripts_dir}/support/support-monitor.sh
-    elif [[ "$managed" =~ ^[nN]$ ]]; then
-        printf "  ${WT}$USER ${OG}has selected to remain in Monitor mode."
-        printf "  ${CY}You can toggle Monitor mode On/Off in the Easy Linux Hacking Menu."
-        echo
-    else
-        clear
-    fi
+    printf "${CY}  Disable Monitor mode in the Easy Linux Hacking menu.\\n"
 fi
 
-echo
+printf " \\n"
 printf "${GN}\\n*********************************** Exiting ************************************\\n"
 printf "             ${WT}--  ${GN}Thanks for using Beesoc's Easy Linux Loader  ${WT}--\\n\\n"
 printf "           ${CY}Use the icon on your ${WT}Apps menu ${CY}or enter [ ${WT}easy-linux.sh${CY} ]\\n"
