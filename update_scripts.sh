57f45b0a5cca74c0a91c3c65bd6a757d78248c8c
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-26 15:47:19 -0500

    Update update_scripts.sh

--- update_scripts.sh
+++ update_scripts.sh
@@ -2,7 +2,7 @@
 # update-scripts.sh
 scripts_dir=/opt/easy-linux
 set -e
-# Version: 0.0.3
+# Version: 0.0.4
 trap ${scripts_dir}/support/trap-master.sh EXIT
 # Read the required versions from version-easy-linux.sh
 source "${scripts_dir}/version-easy-linux.sh"
