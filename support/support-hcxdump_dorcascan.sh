b4d5882dec8168640626fe2d40e471fef703e9ff
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-27 22:36:47 -0500

    Update support-hcxdump_dorcascan.sh

--- support/support-hcxdump_dorcascan.sh
+++ support/support-hcxdump_dorcascan.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-scripts_dir=/opt/easy-linux
 #install and use hcxtools w hashcat and jack
 #
 set -e
 #
-trap ${scripts_dir}/support/support-hcxdump2.sh EXIT
-source ${scripts_dir}/.envrc
+trap /opt/easy-linux/support/support-hcxdump.sh EXIT
 # Version: 0.0.4
 #	timeout -v 32s ${scripts_dir}/support/support-hcxdump2.sh
-	timeout -v 40s sudo hcxdumptool -i "${adapter}" --do_rcascan 
-
+do_rcascan=0
+timeout -v 40s sudo hcxdumptool -i "${adapter}" --do_rcascan &
+timeout -v 38s echo "export do_rcascan=1"
 sleep 1
+source /opt/easy-linux/support/support-hcxdump.sh
 #clear
 printf "    ${OG}Restarting NetworkManager and wpa_supplicant..."
 sudo systemctl start NetworkManager
