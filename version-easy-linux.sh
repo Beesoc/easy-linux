24a90f74734dd5a048d6e355a20945bb143f92e9
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-27 21:43:28 -0500

    Update version-easy-linux.sh

--- version-easy-linux.sh
+++ version-easy-linux.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # Version: 0.0.4
 set -e
-scripts_dir=/opt/easy-linux
-source ${scripts_dir}/.envrc
-#clear
-#source "${scripts_dir}/support/support-Banner_func.sh"
 # Define the minimum required version for each script
 declare -A required_versions=(
 	["install/menu-master.sh"]="0.0.3"
@@ -54,47 +50,3 @@ declare -A required_versions=(
 	["support/support-wpaDOWN.sh"]="0.0.3"
 	["support/trap-master.sh"]="0.0.3"
 )
-
-#function check_version {
-#	script_path=$1
-#	script_name=$(basename "$script_path")
-#	current_version=$(grep '^# Version:' "$script_path" | awk '{print $3}')
-#	required_version=${required_versions[$script_name]}
-#
-#	if [[ "$(printf '%s\n' "$required_version" "$current_version" | sort -V | head -n1)" != "$required_version" ]]; then
-#		printf "${RED}Error: $script_path version $required_version or higher is required (current version is $current_version)" >&2
-#		exit 1
-#	fi
-#}
-
-#function update_script {
-#	script_name=$1
-#	script_path=$2
-#	latest_version=$(curl -s https://raw.githubusercontent.com/Beesoc/easy-linux/main/"$script_name" | grep '^# Version:' | awk '{print $3}')
-#
-#	if [[ "$(printf '%s\n' "$latest_version" "${required_versions[$script_name]}" | sort -V | head -n1)" == "$latest_version" ]]; then
-#		echo "Updating $script_name to version $latest_version"
-#		curl -s https://raw.githubusercontent.com/Beesoc/easy-linux/main/"$script_name" >"$script_path"
-#		chmod +x "$script_path"
-#	fi
-#}
-
-#for script_path in $(find . -type f -name "*.sh"); do
-#	check_version "$script_path"
-#done
-
-#for script_path in $(find . -type f -name "*.sh"); do
-#	script_name=$(basename "$script_path")
-#	update_script "$script_name" "$script_path"
-#done
-
-#for script_path in $(find . -type f -name "*.zip"); do
-#	check_version "$script_path"
-#done
-
-#for script_path in $(find . -type f -name "*.zip"); do
-#	script_name=$(basename "$script_path")
-#	update_script "$script_name" "$script_path"
-#done
-
-#printf "    ${GN}All scripts are up to date.\\n"
