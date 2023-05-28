9885a74f1f99e85971958a32db120dad28867c94
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-27 23:59:30 -0500

    Update support-hcxdump_dorcascan.sh

--- support/support-hcxdump_dorcascan.sh
+++ support/support-hcxdump_dorcascan.sh
@@ -6,7 +6,6 @@ set -e
 trap /opt/easy-linux/support/support-hcxdump.sh EXIT
 # Version: 0.0.4
 #	timeout -v 32s ${scripts_dir}/support/support-hcxdump2.sh
-do_rcascan=0
 timeout -v 40s sudo hcxdumptool -i "${adapter}" --do_rcascan &
 timeout -v 38s echo "export do_rcascan=1"
 sleep 1
