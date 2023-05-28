8313f25dd5088b29ecebe030725f42ca122ebadd
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-27 22:40:49 -0500

    Update and rename support-hcxdump3.sh to support-hcxdump_full.sh

--- support/support-hcxdump_full.sh
+++ support/support-hcxdump_full.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 scripts_dir=/opt/easy-linux
+source ${scripts_dir}/.envrc
 #install and use hcxtools w hashcat and jack
-#
+# Version: 0.0.2
 set -e
 #
-trap ${scripts_dir}/support/support-hcxdump2.sh EXIT
-source ${scripts_dir}/.envrc
-# Version: 0.0.4
+trap ${scripts_dir}/support/support-hcxdump.sh EXIT
 #	timeout -v 32s ${scripts_dir}/support/support-hcxdump2.sh
-    
-
-	timeout -v 120s sudo hcxdumptool -i "${adapter}" -f 0 -f 1 -f 2 -f 4 -f 32 --stop_ap_attacks=600 --resume_ap_attacks=864000d --enable_status=31 --weakcandidate=12345678 --flood_beacon 
+timeout -v 120s sudo hcxdumptool -i "${adapter}" -f 0 -f 1 -f 2 -f 4 -f 32 --stop_ap_attacks=600 --resume_ap_attacks=864000d --enable_status=31 --weakcandidate=12345678 --flood_beacon 
   -o ${scripts_dir}/support/misc/hcxdumptool.pcapng
 
 sleep 1
