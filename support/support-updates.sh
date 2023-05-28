71b49aa2bcd1401ab24d9ddf379081f56221baf4
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-27 20:35:46 -0500

    Update support-updates.sh

--- support/support-updates.sh
+++ support/support-updates.sh
@@ -9,6 +9,7 @@ if [[ $USER != "$cwb_username" ]] || [[ $(cat /etc/hostname) != "$cwb_computerna
 	exit 0
 elif [[ $USER = "$cwb_username" ]] && [[ $(cat /etc/hostname) = "$cwb_computername" ]]; then
 	#####  Personal  #######
+if [ ! -d /opt/Storm-Breaker ]; then 
 	printf "${CY}  Install Storm-Breaker"
 	sudo apt install -y python3-requests python3-colorama python3-psutil >/dev/null
 	cd /opt || exit
@@ -30,15 +31,17 @@ elif [[ $USER = "$cwb_username" ]] && [[ $(cat /etc/hostname) = "$cwb_computerna
 );
 
 ?>" >config.php
-
-	printf "Install ngrok"
+fi
+if [ ! -f $HOME/Downloads/ngrok-v3-stable-linux-amd64.tgz ] ; then
+        printf "Install ngrok"
 	cd $HOME/Downloads
 	wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 	sudo tar xvzf $HOME/Downloads/ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
 	printf "Go signup for an ngrok account.  That's how you get the key below"
 	ngrok config add-authtoken $ngrok_token
-
 fi
+fi
+
 
 printf "${CY}  All apps in this script have been successfully installed. This includes all apps listed here: ${PL}\\n"
 printf "$packages\\n"
