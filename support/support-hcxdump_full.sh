dd4241d61f646fef8eb69e6056b14fd060a2747a
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-27 23:58:26 -0500

    Update support-hcxdump_full.sh

--- support/support-hcxdump_full.sh
+++ support/support-hcxdump_full.sh
@@ -6,6 +6,8 @@ source ${scripts_dir}/.envrc
 set -e
 #
 trap ${scripts_dir}/support/support-hcxdump.sh EXIT
+hcxdump_full=1
+
 #	timeout -v 32s ${scripts_dir}/support/support-hcxdump2.sh
 timeout -v 600s sudo hcxdumptool -i "${adapter}" -f 0 -f 1 -f 2 -f 4 -f 32 --stop_ap_attacks=600 --resume_ap_attacks=864000d --enable_status=31 --weakcandidate=12345678 --flood_beacon 
   -o ${scripts_dir}/support/misc/hcxdump_full.pcapng
