2ffc033efd609f71bc3722cc9c6a38d65a64f739
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-29 16:30:47 -0500

    Add files via upload

--- support/support-hcxdump.sh
+++ support/support-hcxdump.sh
@@ -20,10 +20,10 @@ ripper_func() {
   read -n 1 -r ripperchoice
   ripperchoice=${ripperchoice:-2}
 
-  if [[ ${ripperchoice} -eq 1 ]]; then
+  if [[ ${ripperchoice} == 1 ]]; then
     # Crack the hash with John the Ripper using the OneRuleToRuleThemAll.rule
     john --wordlist="${WORDLIST}" --rules="${RULES}" --stdout | aircrack-ng -w - -e SSID /opt/backup/root/handshakes/hashes.22000 -J /opt/backup/root/handshakes/mypots.potfile
-  elif [[ ${ripperchoice} -eq 2 ]]; then
+  elif [[ ${ripperchoice} == 2 ]]; then
     # Crack the hash with hashcat
     hashcat -m 22000 -a 3 -w 4 -o /opt/backup/root/handshakes/mypots.potfile --force --opencl-device-types=1,2 "${WORDLIST}" /opt/backup/root/handshakes/hashes.22000 --increment --increment-min=8 --increment-max=13
   else
@@ -37,24 +37,25 @@ ripper_func() {
 full_scan() {
 clear
 source "${scripts_dir}/support/support-Banner_func.sh"
-if [[ $hcxdump_full -ne 1 ]]; then
-  do_rcascan="0"  
+if ls ${scripts_dir}/support/hcxdump* >/dev/null 2>&1; then
+
+  do_rcascan=0
   printf "${CY}  Whoop. How would you like to do a ${WT}REAL ${CY}scan.\\n"
   printf "${OG}  The longer you wait on the next screen, the more attacks will complete, and the\\n" 
   printf "${OG}  more hashes you will have. It will stop itself after 10 mins or you can press\\n"
   printf "${OG}  ${WT}Ctrl${OG} + ${WT}C ${OG}to stop it early.\\n"
   read -n 1 -r -p "Good things come...Press any key "
   echo "export hcxdump_full=1"
+  sleep 600 &
   source ${scripts_dir}/support/support-hcxdump_full.sh
-  sleep 600
   sudo /usr/bin/hcxpcapngtool -o "${scripts_dir}/support/misc/hcxdump_full.pcapng*"
-
   ripper_func
 elif [[ $hcxdump_full == 1 ]]; then
   printf "${GN}  So...you ran the full scan"; sleep 2; printf " ...I hope it paid off for you."; sleep 2; printf "You'll find out soon\\n"
   sleep 3; printf "...ish"
   echo "export hcxdump_full="
   read -r -p "Press any key to continue."
+  printf "${CY}\\n"
   ripper_func
 fi
 }
@@ -94,10 +95,14 @@ choice_func() {
   source "${scripts_dir}/support/support-Banner_func.sh"
   printf "${OG}   \\n"
 
-  sudo systemctl stop NetworkManager && sudo systemctl stop wpa_supplicant
-  echo
+  printf "  ${CY}On the next screen, you will be asked to pick which wifi adapter to use.\\n"
+  printf "  ${CY}Your selected wifi adapter will be unavailable (ex: no internet) while scanning.\\n"
+  printf "  ${OG}You need to ${WT}pick Monitor mode ${OG}on the next screen to start hacking.${YW}\\n"
+  read -n 1 -r -p "Press any key to continue. " pressany
+  source ${scripts_dir}/support/support-monitor.sh
   printf " \\n${WT}"
-  printf "  ${CY}Press ${WT}[S] ${CY}to scan...Please ${RED}do not interrupt for 40 sec ${CY}scan...${OG}\\n"
+  printf "${CY}  Now we need to do our initial ${WT}[S]${CY}can for targets or ${WT}[T]${CY}est your wifi adapter's monitor functions.\\n" 
+  printf "  ${CY}Press ${WT}[S]${CY}can...Please ${RED}do not interrupt for 40 sec ${CY}scan...${OG}\\n"
   echo
   printf "  Scan will stop automatically when complete...\\n${WT}   "
   printf "${GN} \\n"
@@ -105,47 +110,46 @@ choice_func() {
   read -n 1 -r -p "[T]est or [S]can [t/S] ----> " torscan
   torscan=${torscan:-S}
   printf "\\n"
-  
   if [[ "${torscan}" = "T" ]] || [[ "${torscan}" = "t" ]]; then
     Test_func
   elif [[ "${torscan}" = "S" ]] || [[ "${torscan}" = "s" ]]; then
     printf " \\n"
-       
-      if (ls "${scripts_dir}/support/misc/hcxdumptool*" >/dev/null 2>&1); then
-      cd "${scripts_dir}/support/misc" || exit
-      printf "${OG}  Hcxdump files exist in your misc/ directory. Using them.\\n"
-      sleep 3
-      sudo chown -vR $USER:0 "${scripts_dir}/support/misc"
-      sudo /usr/bin/hcxpcapngtool -o "${scripts_dir}/support/misc/*.pcapng*"
-      sudo cp ${scripts_dir}/support/misc/*.pcapng ${scripts_dir}/support
-      sudo rm -f ${scripts_dir}/support/misc/*.pcapng
-   #   cat 
-   #   rm -f ./*pcapng*
-      read -n 1 -p "Done with hcxpcapng RCAScanning. Press any key. \\n"
-      full_scan
-      elif (! ls "${scripts_dir}/support/misc/hcxdumptool*" >/dev/null 2>&1); then
-      # Code to execute when the files don't exist
-      printf "${OG}  You have selected scan. The next step will take 40 secs.\\n"
-      printf "${OG}  Do ${WT}NOT ${OG}interrupt until the scan has completed.\\n  "
-      read -n 1 -r -p "Press any key to continue." anykey2
-        sleep 41 &
-        source "${scripts_dir}/support/support-hcxdump_dorcascan.sh"
-      printf "\\n"
-      sudo chown -vR $USER:0 "${scripts_dir}/support/misc"
-      sudo /usr/bin/hcxpcapngtool -o "${scripts_dir}/support/misc/*.pcapng*"
-      sudo cp ${scripts_dir}/support/misc/*.pcapng ${scripts_dir}/support
-      sudo rm -f ${scripts_dir}/support/misc/*.pcapng
-      full_scan
-      elif $(ls "${scripts_dir}/support/hcxdump*" >/dev/null 2>&1); then
-      full_scan
-      fi
+
+        if ls "${scripts_dir}/support/misc/hcxdumptool"* >/dev/null 2>&1 || ls "${scripts_dir}/support/hcxdump"* >/dev/null 2>&1; then
+            if ls "${scripts_dir}/support/hcxdump"* >/dev/null 2>&1; then
+              full_scan
+            elif ! ls "${scripts_dir}/support/hcxdump"* >/dev/null 2>&1; then
+             cd "${scripts_dir}/support/misc" || exit
+             printf "${OG}  Hcxdump files exist in your misc/ directory. Using them.\\n"
+             sleep 3
+             sudo chown -vR $USER:0 "${scripts_dir}/support/misc"
+             sudo /usr/bin/hcxpcapngtool -o "${scripts_dir}/support/"*.pcapng*
+             sudo cp "${scripts_dir}/support/misc/"*.pcapng "${scripts_dir}/support"
+             sudo rm -f "${scripts_dir}/support/misc/"*.pcapng
+             read -n 1 -p "Done with hcxpcapng RCAScanning. Press any key. \\n"
+             full_scan
+             fi
+      elif ! ls "${scripts_dir}/support/misc/hcxdumptool"* >/dev/null 2>&1 || ! ls "${scripts_dir}/support/hcxdump"* >/dev/null 2>&1; then
+        # Code to execute when the files don't exist
+           printf "${OG}  You have selected scan. The next step will take 40 secs.\\n"
+           printf "${OG}  Do ${WT}NOT ${OG}interrupt until the scan has completed.\\n  "
+           read -n 1 -r -p "Press any key to continue." anykey2
+           sleep 41 &
+           source "${scripts_dir}/support/support-hcxdump_dorcascan.sh"
+           printf "\\n"
+           sudo chown -vR $USER:0 "${scripts_dir}/support/misc"
+           sudo /usr/bin/hcxpcapngtool -o ${scripts_dir}/support/misc/*.pcapng*
+           sudo cp ${scripts_dir}/support/misc/*.pcapng ${scripts_dir}/support
+           sudo rm -f ${scripts_dir}/support/misc/*.pcapng
+           full_scan
+        fi
 else
       printf "${RED}  Invalid Selection.  Press ${WT}S ${RED}to scan, or ${WT}T ${RED}to test wifi adapters."
 fi
 }
 
 main() {
-  source "${scripts_dir}/support/support-netadapter.sh"
+  source "${scripts_dir}/support/support-monitor.sh"
   # Capture the hashes with hcxdumptool
   choice_func
   ripper_func
