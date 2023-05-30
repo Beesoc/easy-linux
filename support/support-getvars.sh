16cf7b800cc09dacdbb632dd219089d1a478ff74
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-30 12:24:31 -0500

    Update support-getvars.sh

--- support/support-getvars.sh
+++ support/support-getvars.sh
@@ -17,16 +17,16 @@ reg-user_func() {
 	printf "\\n${CY}The user with ${WT}UID 1000 ${CY}is: \\n"
 	printf "${GN}  Username: ${WT}$reg_user \\n"
 
-	if [[ $reg_user = $USER && $USER = $username ]]; then
+	if [[ $reg_user = "$USER" && $USER = "$username" ]]; then
 		printf "${CY}  Usernames ${WT}${USER}${CY}, ${WT}${username}${CY}, and ${WT}${reg_user} ${CY}all match.\\n  "
-	elif [[ $reg_user != $USER || $USER != $username || $reg_user != $username ]]; then
+	elif [[ $reg_user != "$USER" || $USER != "$username" || $reg_user != "$username" ]]; then
 		printf "${RED}  Usernames ${WT}${USER}${RED}, ${WT}${username}${RED}, and ${WT}${reg_user} ${RED}don't match.\\n\\n  "
 		printf "${RED}  Username mismatch! Press ${WT}any ${RED}"
-		read -n 1 -p "key to continue."
+		read -n 1 -r -p "key to continue."
 	fi
 
 	printf "\\n${CY}  Press ${WT}any ${CY}"
-	read -n 1 -p "key to continue."
+	read -n 1 -r -p "key to continue."
 }
 
 main() {
