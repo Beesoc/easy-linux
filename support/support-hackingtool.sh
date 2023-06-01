f93dcfac58c69d299d160da607087db34911aa67
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-06-01 18:08:48 -0500

    Add files via upload

--- support/support-hackingtool.sh
+++ support/support-hackingtool.sh
@@ -5,38 +5,47 @@ set -e
 source "${scripts_dir}/.envrc"
 clear
 source "${scripts_dir}/support/support-Banner_func.sh"
-printf "${WT}\\n\\        You chose Hacking Tool. ${CY}Download and use the hacking tool featured\\n"
+printf "${WT}\\n        You chose Hacking Tool. ${CY}Download and use the hacking tool featured\\n"
 printf "          in Mr. Robot. This tool has MANY tools built into it.  ${GN}This is a\\n"
 printf "       definite must install. ${WT}Swiss army knife for hackers.\\n\\n    "
 
-if [[ $(command -v hackingtool >/dev/null 2>&1) ]]; then
+if command -v hackingtool >/dev/null 2>&1; then
 	printf "\\n${GN}Hackingtool is already installed.\\n"
 	sudo hackingtool
 else
-	printf "\\n${YW}hackingtool is not installed.  Installing...\\n"
-fi
+	printf "\\n${YW}hackingtool is not installed.\\n"
+
 
 read -n 1 -p "Do you want to install HackingTool [Y/n] " choicehacking
 choicehacking=${choicehacking:-Y}
-if [[ "$choicehacking" =~ ^[Yy]$ ]]; then
-	printf "${GN}Continuing...\\n"
-else
-	printf "${RED}Exiting.\\n"
-	exit 0
-fi
+    if [[ "$choicehacking" =~ ^[nN]$ ]]; then
+    	printf "${RED}Exiting\\n"
+         exit 0
+    elif [[ "$choicehacking" =~ ^[yY]$ ]]; then
+    	printf "${WT}Continuing.\\n"
 
-if [[ -d ${compiled_dir} ]]; then
-	cd $HOME/compiled || exit
-	return 1
-elif [[ ! -d ${compiled_dir} ]]; then
-	mkdir $HOME/compiled
-	cd $HOME/compiled || exit
-fi
+
+        if [[ -d ${compiled_dir} ]]; then
+        	cd $HOME/compiled || exit
+                if [[ -d $HOME/compiled/hackingtool ]]; then
+                    printf "${OG}  Existing hackingtool folder found. Removing/re-cloning.\\n${NC}"
+                    sudo rm -rf $HOME/compiled/hackingtool
+                fi
+        elif [[ ! -d ${compiled_dir} ]]; then
+        	mkdir $HOME/compiled
+	        cd $HOME/compiled || exit
+        fi
+    
 printf " \\n${CY}"
 sudo git clone https://github.com/Z4nzu/hackingtool.git
 cd hackingtool || exit
+
+source /usr/lib/python3.11/venv/scripts/common/activate
 sudo pip3 install -r requirements.txt
-source ./install.sh
+sudo bash ./install.sh
+   fi
+fi
 printf "${CY}  Press ${WT}any ${CY}key to continue."
 read -n 1 -s -t 300
-source ${scripts_dir}/install/menu-apps.sh
+
+sudo hackingtool
