ffa2c7846c3260876e13c737929d7c0b8766b725
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-06-13 03:29:11 -0500

    Update menu-containerized.sh

--- install/menu-containerized.sh
+++ install/menu-containerized.sh
@@ -15,7 +15,59 @@ _func() {
 	read -n 1 -r
 }
 
+emulatorjs_func() {
+# Install EmulatorJS
+container_name="emulatorjs"
+
+# Check if the container is running
+if docker ps --format '{{.Names}}' | grep -q "^${container_name}\$"; then
+  echo "Container '${container_name}' is already running."
+  printf "${OG}Do you want to launch the ${WT}[b]${OG}ackend config or the ${WT}[f]${OG}rontend to play? [b/F] "
+  read -n 1 -r choice
+  choice=${choice:-F}
+  if [[ "${choice}" ^[fF]$ ]]; then
+  	xdg-open http://localhost:83
+  elif [[ "${choice}" ^[bB]$ ]]; then
+  	xdg-open http://localhost:3003
+  else
+      printf "${RED} Invalid Selection.  Choices are F or B.\\n"
+  fi
+else
+  echo "Container '${container_name}' is not running."
+   if [[ ! -d /opt/appdata ]]; then
+	sudo mkdir /opt/appdata
+   elif [[ ! -d /opt/appdata/emulatorjs ]]; then
+	sudo mkdir /opt/appdata/emulatorjs
+   elif [[ ! -d /opt/appdata/roms ]]; then
+	sudo mkdir /opt/appdata/roms
+   else
+docker run -d --name=emulatorjs \
+  -e PUID=1000 -e PGID=1000 -e TZ=America/Chicago -e SUBFOLDER=/ `#optional` \
+  -p 3003:3000 -p 83:80 -p 4001:4001 `#optional` \
+  -v /opt/appdata/emulatorjs:/config -v /opt/appdata/roms/:/data --restart unless-stopped 
+  lscr.io/linuxserver/emulatorjs:latest
+   fi
+	printf "${CT}  Press ${WT}any ${CT}key to continue. \\n  ${OG}"
+	read -n 1 -r
+fi
+}
+
 flaresolverr_func() {
+container_name=flaresolverr
+if docker ps --format '{{.Names}}' | grep -q "^${container_name}\$"; then
+  echo "Container '${container_name}' is already running."
+  printf "${OG}Do you want to launch ${WT}${container_name} ${OG}container now [Y/n] \\n"
+  read -n 1 -r choice
+  choice=${choice:-Y}
+  if [[ "${choice}" ^[yY]$ ]]; then
+  	sudo flaresolverr
+  elif [[ "${choice}" ^[nN]$ ]]; then
+  	printf "${RED} $USER selected not to launch ${container_name}.  Exiting.\\n"
+  else
+      printf "${RED} Invalid Selection.  Choices are Y or N.\\n"
+  fi
+else
+  echo "Container '${container_name}' is not running."
 docker run -d \
   --name=flaresolverr \
   -p 8191:8191 \
@@ -24,6 +76,7 @@ docker run -d \
   ghcr.io/flaresolverr/flaresolverr:latest
 	printf "${CT}  Press ${WT}any ${CT}key to continue. \\n  ${OG}"
 	read -n 1 -r
+fi
 }
 
 hydra_func() {
@@ -45,9 +98,32 @@ hashcat_func() {
 }
 
 glances_func() {
+container_name=glances
+
+if docker ps --format '{{.Names}}' | grep -q "^${container_name}\$"; then
+  echo "Container '${container_name}' is already running."
+  printf "${OG}Do you want to launch ${WT}${container_name} ${OG}container now [Y/n] \\n"
+  read -n 1 -r choice
+  choice=${choice:-Y}
+  if [[ "${choice}" ^[yY]$ ]]; then
+  	sudo glances
+  elif [[ "${choice}" ^[nN]$ ]]; then
+  	printf "${RED} $USER selected not to launch ${container_name}.  Exiting.\\n"
+  else
+      printf "${RED} Invalid Selection.  Choices are Y or N.\\n"
+  fi
+else
+  echo "Container '${container_name}' is not running."
+   if [[ ! -d /opt/appdata ]]; then
+	sudo mkdir /opt/appdata
+   elif [[ ! -d /opt/appdata/glances ]]; then
+	sudo mkdir /opt/appdata/emulatorjs
+   else
       docker pull nicolargo/glances:latest-full
+
       printf "${CT}  Press ${WT}any ${CT}key to continue. \\n  ${OG}"
 	read -n 1 -r
+   fi
 }
 
 main_menu_func() {
@@ -75,7 +151,7 @@ main_menu() {
 	printf "   ${WT}11)${CY}  ${PL}: \n"
 	printf "   ${WT}12)${CY}  ${PL}: \n"
 	printf "   ${WT}13)${CY}  Flaresolverr${PL}: Cloudflare proxy for the arrr apps\n"
-	printf "   ${WT}14)${CY}  ${PL}: \n"
+	printf "   ${WT}14)${CY}  EmulatorJS${PL}: \n"
       printf "   ${WT}15)${GN}  Return to Main Menu${PL}\n"
 	printf "   ${WT}16)${RED}  [✘] Exit tool [✘]: Uhh, it just exits.\n"
 	echo
@@ -95,7 +171,7 @@ main_menu() {
 	11) _func ;;
 	12) _func ;;
 	13) flaresolverr_func ;;
-	14) _func ;;
+	14) emulatorjs_func ;;
 	15) main_menu_func ;;
 	16) exit 0 ;;
 	*) printf "${RED}Invalid selection.${CY}\n" ;;
