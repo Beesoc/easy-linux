816b69ea1f0720b43a2bd171862fb224c0c29fa9
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-07-03 19:11:06 -0500

    Update support-docker-launcher.sh

--- support/support-docker-launcher.sh
+++ support/support-docker-launcher.sh
@@ -102,7 +102,7 @@ if [[ "${container}" != "diyhue" ]]; then docker_up_func; fi
 }
 
 network_func() {
-        if [[ ! $(docker network ls | grep "lsio") >/dev/null ]]; then
+        if [[ ! -d $(docker network ls | grep "lsio") >/dev/null ]]; then
                 sudo docker network create ${defnet}
         fi
 }
